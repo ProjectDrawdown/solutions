3575ffdb5d0a9868f2bf3c8559267e78901f0698
Owen Barton <owen.barton@civicactions.com>
2018-09-17 18:11:27 -0700

    Final cleanup from rebasing.

--- model/tests/test_unitadoption.py
+++ model/tests/test_unitadoption.py
@@ -9,96 +9,92 @@ from model import unitadoption
 class TestUnitAdoption(unittest.TestCase):
     """Tests for unitadoption.py functionality."""
 
+    def test_na_funits(self):
+        '''Test net adoption functional units calculation.'''
 
-def test_na_funits():
-    '''Test net adoption functional units calculation.'''
+        columns = ["World", "OECD90", "Eastern Europe"]
+        ref_sol_funits = pd.DataFrame.from_dict(
+            dict([(2014, [112.63, 75.00,  0.33]),
+                  (2015, [117.07, 75.63,  0.34]),
+                  (2016, [121.51, 76.25,  0.34])]),
+            orient='index', columns=columns)
+        pds_sol_funits = pd.DataFrame.from_dict(
+            dict([(2014, [112.63, 75.00, 0.33]),
+                  (2015, [176.24, 0, 0]),
+                  (2016, [272.03, 0, 0])]),
+            orient='index', columns=columns)
+        na_funits = pd.DataFrame.from_dict(
+            dict([(2014, [0, 0, 0]),
+                  (2015, [59.17, -75.63, -0.34]),
+                  (2016, [150.52, -76.25, -0.34])]),
+            orient='index', columns=columns)
+        ua = unitadoption.UnitAdoption()
+        result = ua.na_funits(ref_sol_funits, pds_sol_funits)
+        # Confirm that values are equal across the entire array.
+        pd.testing.assert_frame_equal(result, na_funits, check_exact=False,
+                                      check_less_precise=2)
 
-    columns = ["World", "OECD90", "Eastern Europe"]
-    ref_sol_funits = pd.DataFrame.from_dict(
-        dict([(2014, [112.63, 75.00,  0.33]),
-              (2015, [117.07, 75.63,  0.34]),
-              (2016, [121.51, 76.25,  0.34])]),
-        orient='index', columns=columns)
-    pds_sol_funits = pd.DataFrame.from_dict(
-        dict([(2014, [112.63, 75.00, 0.33]),
-              (2015, [176.24, 0, 0]),
-              (2016, [272.03, 0, 0])]),
-        orient='index', columns=columns)
-    na_funits = pd.DataFrame.from_dict(
-        dict([(2014, [0, 0, 0]),
-              (2015, [59.17, -75.63, -0.34]),
-              (2016, [150.52, -76.25, -0.34])]),
-        orient='index', columns=columns)
-    ua = unitadoption.UnitAdoption()
-    result = ua.na_funits(ref_sol_funits, pds_sol_funits)
-    # Confirm that values are equal across the entire array.
-    pd.testing.assert_frame_equal(result, na_funits, check_exact=False,
-                                  check_less_precise=2)
+    def test_life_rep_years(self):
+        """Test Lifetime Replacement for solution/conventional in years calculation."""
 
+        ua = unitadoption.UnitAdoption()
+        result = ua.life_rep_years(48343.80, 1841.67)
+        self.assertAlmostEqual(result, 26.00)
 
-def test_life_rep_years(self):
-    """Test Lifetime Replacement for solution/conventional in years calculation."""
+    def test_sol_cum_iunits(self):
+        '''Test cumulative solution implementation units installed'''
 
-    ua = unitadoption.UnitAdoption()
-    result = ua.life_rep_years(48343.80, 1841.67)
-    self.assertAlmostEqual(result, 26.00)
+        columns = ["World", "OECD90", "Eastern Europe"]
+        ref_sol_funits = pd.DataFrame.from_dict(
+            dict([(2014, [112.63, 75.00,  0.33]),
+                  (2015, [117.07, 75.63,  0.34]),
+                  (2016, [121.51, 76.25,  0.34])]),
+            orient='index', columns=columns)
+        aau_sol_funits = 1841.67
+        ref_sol_cum_iunits = pd.DataFrame.from_dict(
+            dict([(2014, [0.0611, 0.0407, 0.000179]),
+                  (2015, [0.0635, 0.0410, 0.000184]),
+                  (2016, [0.0659, 0.0414, 0.000184])]),
+            orient='index', columns=columns)
+        ua = unitadoption.UnitAdoption()
+        result = ua.sol_cum_iunits(
+            ref_sol_funits, aau_sol_funits)
+        # Confirm that values are equal across the entire array.
+        pd.testing.assert_frame_equal(result, ref_sol_cum_iunits, check_exact=False,
+                                      check_less_precise=2)
 
+    def test_sol_ann_iunits(self):
+        '''Test new implementation units required (including replacement units)'''
 
-def test_sol_cum_iunits():
-    '''Test cumulative solution implementation units installed'''
+        # Test only the cumulative function - the lifetime should have no effect here.
+        # For OEC90, this also tests that negative change in units is not counted.
+        columns = ["World", "OECD90", "Eastern Europe"]
+        ref_sol_cum_iunits = pd.DataFrame.from_dict(
+            dict([(2014, [0.06116, 0.04073, 0.00018]),
+                  (2015, [0.06357, 0.04106, 0.00018]),
+                  (2016, [0.06598, 0.03, 0.00015])]),
+            orient='index', columns=columns)
+        life_rep_sol_years = 26.00
+        ref_sol_ann_funits_diff = pd.DataFrame.from_dict(
+            dict([(2015, [0.00241, 0.00033, 0.0]),
+                  (2016, [0.00241, 0.0, 0.0])]),
+            orient='index', columns=columns)
+        ua = unitadoption.UnitAdoption()
+        result = ua.sol_ann_iunits(
+            ref_sol_cum_iunits, life_rep_sol_years)
+        # Confirm that values are equal across the entire array.
+        pd.testing.assert_frame_equal(result, ref_sol_ann_funits_diff, check_exact=False,
+                                      check_less_precise=5)
 
-    columns = ["World", "OECD90", "Eastern Europe"]
-    ref_sol_funits = pd.DataFrame.from_dict(
-        dict([(2014, [112.63, 75.00,  0.33]),
-              (2015, [117.07, 75.63,  0.34]),
-              (2016, [121.51, 76.25,  0.34])]),
-        orient='index', columns=columns)
-    aau_sol_funits = 1841.67
-    ref_sol_cum_iunits = pd.DataFrame.from_dict(
-        dict([(2014, [0.06, 0.04, 0.00]),
-              (2015, [0.06, 0.04, 0.00]),
-              (2016, [0.07, 0.04, 0.00])]),
-        orient='index', columns=columns)
-    ua = unitadoption.UnitAdoption()
-    result = ua.sol_cum_iunits(
-        ref_sol_funits, aau_sol_funits).round(FLOAT_ROUND)
-    # Confirm that values are equal across the entire array.
-    pd.testing.assert_frame_equal(result, ref_sol_cum_iunits, check_exact=False,
-                                  check_less_precise=2)
-
-
-def test_sol_ann_iunits():
-    '''Test new implementation units required (including replacement units)'''
-
-    # Test only the cumulative function - the lifetime should have no effect here.
-    # For OEC90, this also tests that negative change in units is not counted.
-    columns = ["World", "OECD90", "Eastern Europe"]
-    ref_sol_cum_iunits = pd.DataFrame.from_dict(
-        dict([(2014, [0.06116, 0.04073, 0.00018]),
-              (2015, [0.06357, 0.04106, 0.00018]),
-              (2016, [0.06598, 0.03, 0.00015])]),
-        orient='index', columns=columns)
-    life_rep_sol_years = 26.00
-    ref_sol_ann_funits_diff = pd.DataFrame.from_dict(
-        dict([(2015, [0.00241, 0.00033, 0.0]),
-              (2016, [0.00241, 0.0, 0.0])]),
-        orient='index', columns=columns)
-    ua = unitadoption.UnitAdoption()
-    result = ua.sol_ann_iunits(
-        ref_sol_cum_iunits, life_rep_sol_years).round(5)
-    # Confirm that values are equal across the entire array.
-    pd.testing.assert_frame_equal(result, ref_sol_ann_funits_diff, check_exact=False,
-                                  check_less_precise=2)
-
-    # Test a 2 year lifetime replacement - 2016 should now include
-    # units from 2015, as those now need to be replaced.
-    life_rep_sol_years = 1.00
-    ref_sol_ann_funits_lifetime = pd.DataFrame.from_dict(
-        dict([(2015, [0.00241, 0.00033, 0.0]),
-              (2016, [0.00482, 0.00033, 0.0])]),
-        orient='index', columns=columns)
-    result = ua.sol_ann_iunits(
-        ref_sol_cum_iunits, life_rep_sol_years).round(5)
-    # Confirm that values are equal across the entire array.
-    pd.testing.assert_frame_equal(result, ref_sol_ann_funits_lifetime, check_exact=False,
-                                  check_less_precise=2)
+        # Test a 2 year lifetime replacement - 2016 should now include
+        # units from 2015, as those now need to be replaced.
+        life_rep_sol_years = 1.00
+        ref_sol_ann_funits_lifetime = pd.DataFrame.from_dict(
+            dict([(2015, [0.00241, 0.00033, 0.0]),
+                  (2016, [0.00482, 0.00033, 0.0])]),
+            orient='index', columns=columns)
+        result = ua.sol_ann_iunits(
+            ref_sol_cum_iunits, life_rep_sol_years)
+        # Confirm that values are equal across the entire array.
+        pd.testing.assert_frame_equal(result, ref_sol_ann_funits_lifetime, check_exact=False,
+                                      check_less_precise=5)
