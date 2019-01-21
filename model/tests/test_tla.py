a3b30c99c0c96e0441eea5323db1786dda22499d
benibienz <benibienz@gmail.com>
2019-01-21 12:36:50 +0000

    Changed TLA to CustomTLA. Reworked implementation so we can loop through regions. Updated test.

--- model/tests/test_tla.py
+++ model/tests/test_tla.py
@@ -9,41 +9,16 @@ import pytest
 from model import tla
 
 # arguments used in Tropical Forest Restoration v1.1d 27Aug18
-tlaconfig_list = [
-  ['param', 'World', 'PDS World', 'OECD90', 'Eastern Europe', 'Asia (Sans Japan)',
-   'Middle East and Africa', 'Latin America', 'China', 'India', 'EU', 'USA'],
-  ['source_until_2014', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES',
-   'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES'],
-
-  # Not sure about this
-  # ['source_after_2014', 'Baseline Cases',
-  #   'Drawdown TAM: Drawdown TAM - Post Integration - Optimum Scenario', 'ALL SOURCES',
-  #   'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES',
-  #   'ALL SOURCES', 'ALL SOURCES'],
-
-  # (placeholder)
-  ['source_after_2014', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES',
-   'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES'],
-
-  ['trend', '3rd Poly', '3rd Poly', 'Linear', 'Linear', 'Linear', 'Linear', 'Linear',
-   'Linear', 'Linear', 'Linear', 'Linear'],
-  ['growth', 'Medium', 'Medium', 'Medium', 'Medium', 'Medium', 'Medium', 'Medium', 'Medium',
-   'Medium', 'Medium', 'Medium'],
-  ['low_sd_mult', 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
-  ['high_sd_mult', 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
-g_tlaconfig = pd.DataFrame(tlaconfig_list[1:], columns=tlaconfig_list[0]).set_index('param')
 
 datadir = pathlib.Path(__file__).parents[2]
 
-# This is a custom data source specific to solution
-g_tla_ref_data_sources = {
-  'Based on- WRI 2016': str(
-      datadir.joinpath('solution', 'tropicalforests', 'tla_based_on_WRI_2016_widescale_reforestation.csv')),
+g_tla_ref_data_source = {
+    'Based on- WRI 2016': str(
+        datadir.joinpath('solution', 'tropicalforests', 'tla_based_on_WRI_2016_widescale_reforestation.csv')),
 }
 
 def test_forecast_data_global():
-  tl = tla.TLA(tlaconfig=g_tlaconfig, tla_ref_data_sources=g_tla_ref_data_sources)
-  forecast = tl.forecast_data_global()
-  wri = 'Based on- WRI 2016'
-  assert forecast.loc[2060, wri] == pytest.approx(304.0)
+  tl = tla.CustomTLA(g_tla_ref_data_source)
+  global_tla = tl.tla_data_global()
+  assert global_tla.loc[2060, 'TLA'] == pytest.approx(304.0)
 
--- model/tla.py
+++ model/tla.py
@@ -1,9 +1,9 @@
 """
-Total Land Area module.
+TLA Data module.
 Note: in the spreadsheet most of this module is a relic of the TAM module in the RRS model.
 All the statistical analysis is unused as TLA remains constant and has already been allocated by
 DD. The only real function of the TLA tab is to provide a place to input custom TLA data, which
-can be used instead of DD's allocations.
+can be used instead of DD's allocations. Thus, this class is named CustomTLA.
 """
 
 from functools import lru_cache
@@ -14,107 +14,69 @@ from model import interpolation
 import pandas as pd
 
 
-class TLA:
+class CustomTLA:
 
-    def __init__(self, tlaconfig, tla_ref_data_sources):
-        """TLA module.
-
-           Arguments
-           tlaconfig: Pandas dataframe with columns:
-              'source_until_2014', 'source_after_2014', 'trend', 'growth', 'low_sd_mult', 'high_sd_mult'
-              and rows for each region:
-              'World', 'OECD90', 'Eastern Europe', 'Asia (Sans Japan)', 'Middle East and Africa',
-              'Latin America', 'China', 'India', 'EU', 'USA'
-            tla_ref_data_sources: a dict() of data source names.
-            Used for TLA.
-            For example:
-            {
-              'Source A name: 'filename A'
-              'Source B name: 'filename B'
-              ...
-            }
-              tam_pds_data_sources: as tam_ref_data_sources, for the PDS scenario.
+    def __init__(self, tla_data_source):
         """
-        self.tlaconfig = tlaconfig
-        self.tla_ref_data_sources = tla_ref_data_sources
-        self._populate_forecast_data()
+        Converts Custom TLA data for years 2012 - 2060 into a df.
+        tla_data_source is a dict:
+            {'Source Name': 'Source Filename'}
+        May later be expanded to a dict of multiple sources.
+        """
+        self.tla_data_source = tla_data_source
+        self._populate_tla_data()
 
-    def _populate_forecast_data(self):
-        """Read data files in self.tla_*_data_sources to populate forecast data."""
-        self._forecast_data_global = pd.DataFrame()
-        self._forecast_data_global.name = 'forecast_data_global'
-        self._forecast_data_oecd90 = pd.DataFrame()
-        self._forecast_data_oecd90.name = 'forecast_data_oecd90'
-        self._forecast_data_eastern_europe = pd.DataFrame()
-        self._forecast_data_eastern_europe.name = 'forecast_data_eastern_europe'
-        self._forecast_data_asia_sans_japan = pd.DataFrame()
-        self._forecast_data_asia_sans_japan.name = 'forecast_data_asia_sans_japan'
-        self._forecast_data_middle_east_and_africa = pd.DataFrame()
-        self._forecast_data_middle_east_and_africa.name = 'forecast_data_middle_east_and_africa'
-        self._forecast_data_latin_america = pd.DataFrame()
-        self._forecast_data_latin_america.name = 'forecast_data_latin_america'
-        self._forecast_data_china = pd.DataFrame()
-        self._forecast_data_china.name = 'forecast_data_china'
-        self._forecast_data_india = pd.DataFrame()
-        self._forecast_data_india.name = 'forecast_data_india'
-        self._forecast_data_eu = pd.DataFrame()
-        self._forecast_data_eu.name = 'forecast_data_eu'
-        self._forecast_data_usa = pd.DataFrame()
-        self._forecast_data_usa.name = 'forecast_data_usa'
+    def _populate_tla_data(self):
+        """Read data files in."""
+        self.regions = {
+            'global': pd.DataFrame(),
+            'oecd90': pd.DataFrame(),
+            'eastern_europe': pd.DataFrame(),
+            'asia_sans_japan': pd.DataFrame(),
+            'middle_east_and_africa': pd.DataFrame(),
+            'latin america': pd.DataFrame(),
+            'china': pd.DataFrame(),
+            'india': pd.DataFrame(),
+            'eu': pd.DataFrame(),
+            'usa': pd.DataFrame()
+        }
 
-        for (name, filename) in self.tla_ref_data_sources.items():
+
+        for (name, filename) in self.tla_data_source.items():   # leave this in in case we want multiple sources later
             df = pd.read_csv(filename, header=0, index_col=0, skipinitialspace=True,
                              skip_blank_lines=True, comment='#')
-            self._forecast_data_global.loc[:, name] = df.loc[:, 'World']
-            self._forecast_data_oecd90.loc[:, name] = df.loc[:, 'OECD90']
-            self._forecast_data_eastern_europe.loc[:, name] = df.loc[:, 'Eastern Europe']
-            self._forecast_data_asia_sans_japan.loc[:, name] = df.loc[:, 'Asia (Sans Japan)']
-            self._forecast_data_middle_east_and_africa.loc[:, name] = df.loc[:, 'Middle East and Africa']
-            self._forecast_data_latin_america.loc[:, name] = df.loc[:, 'Latin America']
-            self._forecast_data_china.loc[:, name] = df.loc[:, 'China']
-            self._forecast_data_india.loc[:, name] = df.loc[:, 'India']
-            self._forecast_data_eu.loc[:, name] = df.loc[:, 'EU']
-            self._forecast_data_usa.loc[:, name] = df.loc[:, 'USA']
+
+            self.regions['global'].loc[:, name] = df.loc[:, 'World']
+            self.regions['oecd90'].loc[:, name] = df.loc[:, 'OECD90']
+            self.regions['eastern_europe'].loc[:, name] = df.loc[:, 'Eastern Europe']
+            self.regions['asia_sans_japan'].loc[:, name] = df.loc[:, 'Asia (Sans Japan)']
+            self.regions['middle_east_and_africa'].loc[:, name] = df.loc[:, 'Middle East and Africa']
+            self.regions['latin america'].loc[:, name] = df.loc[:, 'Latin America']
+            self.regions['china'].loc[:, name] = df.loc[:, 'China']
+            self.regions['india'].loc[:, name] = df.loc[:, 'India']
+            self.regions['eu'].loc[:, name] = df.loc[:, 'EU']
+            self.regions['usa'].loc[:, name] = df.loc[:, 'USA']
+
+        for (region, region_df) in self.regions.items():
+            region_df.name = 'source_data_{}'.format(region)
+
+            # copy the source column to use as final TLA, so we have a TLA column for unit adoption to locate
+            # later this column could be a statistical combination of multiple sources
+            region_df.loc[:, 'TLA'] = region_df.ix[:, 0]
 
     @lru_cache()
-    def forecast_data_global(self):
+    def tla_data_global(self):
         """ 'TLA Data'!A644:M693 """
-        return self._forecast_data_global
+        return self.regions['global']
 
 
 if __name__ == '__main__':
-    tlaconfig_list = [
-        ['param', 'World', 'PDS World', 'OECD90', 'Eastern Europe', 'Asia (Sans Japan)',
-         'Middle East and Africa', 'Latin America', 'China', 'India', 'EU', 'USA'],
-        ['source_until_2014', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES',
-         'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES'],
-
-        # Not sure about this
-        # ['source_after_2014', 'Baseline Cases',
-        #   'Drawdown TAM: Drawdown TAM - Post Integration - Optimum Scenario', 'ALL SOURCES',
-        #   'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES',
-        #   'ALL SOURCES', 'ALL SOURCES'],
-
-        # (placeholder)
-        ['source_after_2014', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES',
-         'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES'],
-
-        ['trend', '3rd Poly', '3rd Poly', 'Linear', 'Linear', 'Linear', 'Linear', 'Linear',
-         'Linear', 'Linear', 'Linear', 'Linear'],
-        ['growth', 'Medium', 'Medium', 'Medium', 'Medium', 'Medium', 'Medium', 'Medium', 'Medium',
-         'Medium', 'Medium', 'Medium'],
-        ['low_sd_mult', 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
-        ['high_sd_mult', 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
-    g_tlaconfig = pd.DataFrame(tlaconfig_list[1:], columns=tlaconfig_list[0]).set_index('param')
-
     datadir = pathlib.Path(__file__).parents[1]
 
-    g_tla_ref_data_sources = {
+    g_tla_ref_data_source = {
         'Based on- WRI 2016': str(
             datadir.joinpath('solution', 'tropicalforests', 'tla_based_on_WRI_2016_widescale_reforestation.csv')),
     }
-    tla = TLA(g_tlaconfig, g_tla_ref_data_sources)
-
-    print(tla._forecast_data_global)
-    print(tla._forecast_data_oecd90)
+    tl = CustomTLA(g_tla_ref_data_source)
+    print(tl.tla_data_global())
 
