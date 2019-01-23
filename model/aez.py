b6ac216b8d5afe3217f60ac78ae606b9002abb73
benibienz <benibienz@gmail.com>
2019-01-23 15:44:33 +0000

    populate solution land distribution

--- model/aez.py
+++ model/aez.py
@@ -1,10 +1,12 @@
 """
 AEZ Data module.
-Mostly reproduces the corresponding xls sheet, with a few simplifications. All breakdowns by year have
-been removed as the TLA is assumed fixed for all years on all solutions. Land areas are pulled from
-csvs in the 'land/world' directory, and land allocations from the 'land/allocation' directory.
-These in turn are generated from their corresponding spreadsheets in the 'land' directory. These values
-are fixed across all solutions but if they do need updating the xls sheets can be changed and the CSVs
+Mostly reproduces the corresponding xls sheet, with a few simplifications:
+- All breakdowns by year have been removed as the TLA is assumed fixed for all years on all solutions.
+- 'Solution land data' table has not been replicated as it has no function in the model.
+
+Land areas are pulled from CSVs in the 'land/world' directory, and land allocations from the 'land/allocation'
+directory. These in turn are generated from their corresponding spreadsheets in the 'land' directory. These
+values are fixed across all solutions but if they do need updating the xls sheets can be changed and the CSVs
 can be updated by running the relevant script in the 'tools' directory.
 """
 
@@ -19,17 +21,20 @@ class AEZ:
     """ AEZ Data module """
     def __init__(self, solution):
         self.solution = solution
-        self.thermal_moisture_regimes = ['Tropical-Humid', 'Temperate_Boreal-Humid', 'Tropical-Semi-Arid',
-                                    'Temperate_Boreal-Semi-Arid', 'Global Arid', 'Global Arctic']
+        self.thermal_moisture_regimes = ['Tropical-Humid', 'Temperate/Boreal-Humid', 'Tropical-Semi-Arid',
+                                    'Temperate/Boreal-Semi-Arid', 'Global Arid', 'Global Arctic']
+        self.regions = ['OECD90', 'Eastern Europe', 'Asia (Sans Japan)', 'Middle East and Africa', 'Latin America',
+                        'Global', 'China', 'India', 'EU', 'USA']
         self._populate_solution_la()
         self._get_applicable_zones()
         self._populate_world_la()
+        self._populate_solution_ld()
 
     def _populate_solution_la(self):
         # TODO: drawdown allocation toggle
         df = pd.read_csv(LAND_CSV_PATH.joinpath('aez', 'solution_la_template.csv'), index_col=0)
         df = df.fillna(0)
-        for tmr in df.index:
+        for tmr in self.thermal_moisture_regimes:
             tmr_path = LAND_CSV_PATH.joinpath('allocation', tmr.replace('/', '_'))
             for col in df:
                 if col.startswith('AEZ29'):  # this zone is not included in land allocation
@@ -58,9 +63,27 @@ class AEZ:
         """
         self.world_la_dict = {}
         for tmr in self.thermal_moisture_regimes:
-            df = pd.read_csv(LAND_CSV_PATH.joinpath('world', tmr + '.csv'), index_col=0).drop('Total Area (km2)', 1)
-            self.world_la_dict[tmr] = df.mul(self.solution_la_df.loc[tmr.replace('_', '/')], axis=1) / 10000
+            df = pd.read_csv(LAND_CSV_PATH.joinpath('world', tmr.replace('/', '_') + '.csv'), index_col=0).drop('Total Area (km2)', 1)
+            self.world_la_dict[tmr] = df.mul(self.solution_la_df.loc[tmr], axis=1) / 10000
 
+    def _populate_solution_ld(self):
+        cols = self.thermal_moisture_regimes
+        soln_df = pd.DataFrame(columns=cols, index=self.regions).fillna(0.)
+        for reg in self.regions:
+            for tmr, df in self.world_la_dict.items():
+                if reg == 'Global':  # we will sum these later
+                    soln_df.at[reg, tmr] = sum(soln_df[tmr].values[:5])
+                else:
+                    total_area = 0
+                    for col in df.columns:
+                        if col in self.applicable_zones:
+                            total_area += df.loc[reg, col]
+                    soln_df.at[reg, tmr] = total_area
+
+        soln_df['All'] = soln_df.sum(axis=1)
+        self.solution_ld_df = soln_df
+
+        print(self.solution_ld_df)
 
 if __name__ == '__main__':
     aez = AEZ('Tropical Forest Restoration')
--- model/tests/test_aez.py
+++ model/tests/test_aez.py
@@ -1,21 +1,23 @@
 import pytest
 from model import aez
 
+trr_aez = aez.AEZ('Tropical Forest Restoration')
 
 def test_populate_solution_la():
-    trr_aez = aez.AEZ('Tropical Forest Restoration')
     assert trr_aez.solution_la_df.loc['Tropical-Humid', 'AEZ3: Forest, good, moderate'] == pytest.approx(0.245464949942429)
     zero_rows = sum([trr_aez.solution_la_df.iloc[i].sum() for i in [1, 3, 4, 5]])  # these should be all 0
     assert zero_rows == pytest.approx(0)
 
 
 def test_applicable_zones():
-    trr_aez = aez.AEZ('Tropical Forest Restoration')
     assert len(trr_aez.applicable_zones) == 18
     assert 'AEZ27: Rainfed Cropland, marginal, moderate' in trr_aez.applicable_zones
 
 
 def test_populate_world_la():
-    trr_aez = aez.AEZ('Tropical Forest Restoration')
     tsa_df = trr_aez.world_la_dict['Tropical-Semi-Arid']
     assert tsa_df.loc['China', 'AEZ5: Forest, marginal, minimal'] == pytest.approx(0.6833171383331640)
+
+
+def test_populate_solution_ld():
+    assert trr_aez.solution_ld_df.loc['Global', 'All'] == pytest.approx(303.9980581327790)
