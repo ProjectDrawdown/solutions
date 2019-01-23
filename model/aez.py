f0220fdfef7fe8dd305cdefaf91b070b1898376b
benibienz <benibienz@gmail.com>
2019-01-23 14:53:20 +0000

    populate world land allocation

--- model/aez.py
+++ model/aez.py
@@ -1,3 +1,13 @@
+"""
+AEZ Data module.
+Mostly reproduces the corresponding xls sheet, with a few simplifications. All breakdowns by year have
+been removed as the TLA is assumed fixed for all years on all solutions. Land areas are pulled from
+csvs in the 'land/world' directory, and land allocations from the 'land/allocation' directory.
+These in turn are generated from their corresponding spreadsheets in the 'land' directory. These values
+are fixed across all solutions but if they do need updating the xls sheets can be changed and the CSVs
+can be updated by running the relevant script in the 'tools' directory.
+"""
+
 import pandas as pd
 import pathlib
 
@@ -9,10 +19,14 @@ class AEZ:
     """ AEZ Data module """
     def __init__(self, solution):
         self.solution = solution
+        self.thermal_moisture_regimes = ['Tropical-Humid', 'Temperate_Boreal-Humid', 'Tropical-Semi-Arid',
+                                    'Temperate_Boreal-Semi-Arid', 'Global Arid', 'Global Arctic']
         self._populate_solution_la()
         self._get_applicable_zones()
+        self._populate_world_la()
 
     def _populate_solution_la(self):
+        # TODO: drawdown allocation toggle
         df = pd.read_csv(LAND_CSV_PATH.joinpath('aez', 'solution_la_template.csv'), index_col=0)
         df = df.fillna(0)
         for tmr in df.index:
@@ -25,7 +39,7 @@ class AEZ:
                 total_perc_allocated = la_df.loc[self.solution]['Total % allocated']
                 if total_perc_allocated > 0:
                     df.at[tmr, col] = total_perc_allocated
-        self.perc_la_df = df
+        self.solution_la_df = df
 
     def _get_applicable_zones(self):
         df = pd.read_csv(LAND_CSV_PATH.joinpath('aez', 'solution_aez_matrix.csv'), index_col=0)
@@ -35,6 +49,17 @@ class AEZ:
                 self.applicable_zones.append(col)
             elif val != 'no':
                 raise ValueError('cells in matrix should be "yes" or "no"')
+
+    def _populate_world_la(self):
+        """
+        'AEZ Data'!D353:AG610
+        Combines world land area data with Drawdown's land allocation values. Creates a dict of
+        DataFrames sorted by Thermal Moisture Region.
+        """
+        self.world_la_dict = {}
+        for tmr in self.thermal_moisture_regimes:
+            df = pd.read_csv(LAND_CSV_PATH.joinpath('world', tmr + '.csv'), index_col=0).drop('Total Area (km2)', 1)
+            self.world_la_dict[tmr] = df.mul(self.solution_la_df.loc[tmr.replace('_', '/')], axis=1) / 10000
 
 
 if __name__ == '__main__':
--- model/tests/test_aez.py
+++ model/tests/test_aez.py
@@ -4,8 +4,8 @@ from model import aez
 
 def test_populate_solution_la():
     trr_aez = aez.AEZ('Tropical Forest Restoration')
-    assert trr_aez.perc_la_df.loc['Tropical-Humid', 'AEZ3: Forest, good, moderate'] == pytest.approx(0.245464949942429)
-    zero_rows = sum([trr_aez.perc_la_df.iloc[i].sum() for i in [1, 3, 4, 5]])  # these should be all 0
+    assert trr_aez.solution_la_df.loc['Tropical-Humid', 'AEZ3: Forest, good, moderate'] == pytest.approx(0.245464949942429)
+    zero_rows = sum([trr_aez.solution_la_df.iloc[i].sum() for i in [1, 3, 4, 5]])  # these should be all 0
     assert zero_rows == pytest.approx(0)
 
 
@@ -14,3 +14,8 @@ def test_applicable_zones():
     assert len(trr_aez.applicable_zones) == 18
     assert 'AEZ27: Rainfed Cropland, marginal, moderate' in trr_aez.applicable_zones
 
+
+def test_populate_world_la():
+    trr_aez = aez.AEZ('Tropical Forest Restoration')
+    tsa_df = trr_aez.world_la_dict['Tropical-Semi-Arid']
+    assert tsa_df.loc['China', 'AEZ5: Forest, marginal, minimal'] == pytest.approx(0.6833171383331640)
