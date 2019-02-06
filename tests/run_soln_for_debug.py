8362c3b1ec32a123b7a4097f4d426dbd67ed10e4
benibienz <benibienz@gmail.com>
2019-02-06 16:38:17 -0500

    allow helpertables to pass thru custom PDS adoption data

--- tests/run_soln_for_debug.py
+++ tests/run_soln_for_debug.py
@@ -1,5 +1,6 @@
 """ Scratch file for running solutions in debug mode on Pycharm """
 
 from solution.solarpvutil import SolarPVUtil
+from solution.landfillmethane import LandfillMethane
 
-spv = SolarPVUtil()
+soln = LandfillMethane()
