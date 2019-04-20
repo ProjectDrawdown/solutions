6e56aef22f4c3ae747ddf6d10a4bdd081cb30553
Denton Gentry <dgentry@carboncaptu.re>
2019-04-20 06:38:25 -0700

    Regenerate Regenerative Agricuture.
    Most recent merge to master missed updating Regenerative Agricuture,
    regenerate it using the current make_soln.

--- solution/regenerativeagriculture/__init__.py
+++ solution/regenerativeagriculture/__init__.py
@@ -77,13 +77,20 @@ scenarios = {
       # emissions
       soln_indirect_co2_per_iunit=0.0, 
       conv_indirect_co2_per_unit=0.0, 
+
       tco2eq_reduced_per_land_unit='mean', 
       tco2eq_rplu_rate='Annual', 
-
+      tco2_reduced_per_land_unit=0.0, 
+      tco2_rplu_rate='One-time', 
+      tn2o_co2_reduced_per_land_unit=0.0, 
+      tn2o_co2_rplu_rate='One-time', 
+      tch4_co2_reduced_per_land_unit=0.0, 
+      tch4_co2_rplu_rate='One-time', 
       land_annual_emissons_lifetime=100.0, 
 
       emissions_grid_source='Meta-Analysis', emissions_grid_range='Mean', 
       emissions_use_co2eq=True, 
+      emissions_use_agg_co2eq=True, 
 
       # sequestration
       seq_rate_global=np.nan, 
@@ -136,13 +143,20 @@ scenarios = {
       # emissions
       soln_indirect_co2_per_iunit=0.0, 
       conv_indirect_co2_per_unit=0.0, 
+
       tco2eq_reduced_per_land_unit='mean', 
       tco2eq_rplu_rate='Annual', 
-
+      tco2_reduced_per_land_unit=0.0, 
+      tco2_rplu_rate='One-time', 
+      tn2o_co2_reduced_per_land_unit=0.0, 
+      tn2o_co2_rplu_rate='One-time', 
+      tch4_co2_reduced_per_land_unit=0.0, 
+      tch4_co2_rplu_rate='One-time', 
       land_annual_emissons_lifetime=100.0, 
 
       emissions_grid_source='Meta-Analysis', emissions_grid_range='Mean', 
       emissions_use_co2eq=True, 
+      emissions_use_agg_co2eq=True, 
 
       # sequestration
       seq_rate_global=np.nan, 
@@ -194,13 +208,20 @@ scenarios = {
       # emissions
       soln_indirect_co2_per_iunit=0.0, 
       conv_indirect_co2_per_unit=0.0, 
+
       tco2eq_reduced_per_land_unit='mean', 
       tco2eq_rplu_rate='Annual', 
-
+      tco2_reduced_per_land_unit=0.0, 
+      tco2_rplu_rate='One-time', 
+      tn2o_co2_reduced_per_land_unit=0.0, 
+      tn2o_co2_rplu_rate='One-time', 
+      tch4_co2_reduced_per_land_unit=0.0, 
+      tch4_co2_rplu_rate='One-time', 
       land_annual_emissons_lifetime=100.0, 
 
       emissions_grid_source='Meta-Analysis', emissions_grid_range='Mean', 
       emissions_use_co2eq=True, 
+      emissions_use_agg_co2eq=True, 
 
       # sequestration
       seq_rate_global=np.nan, 
@@ -322,15 +343,15 @@ class RegenerativeAgriculture:
         ch4_ppb_calculator=self.c4.ch4_ppb_calculator(),
         soln_pds_net_grid_electricity_units_saved=self.ua.soln_pds_net_grid_electricity_units_saved(),
         soln_pds_net_grid_electricity_units_used=self.ua.soln_pds_net_grid_electricity_units_used(),
-        soln_pds_direct_co2_emissions_saved=self.ua.soln_pds_direct_co2_emissions_saved(),
-        soln_pds_direct_ch4_co2_emissions_saved=self.ua.soln_pds_direct_ch4_co2_emissions_saved(),
-        soln_pds_direct_n2o_co2_emissions_saved=self.ua.soln_pds_direct_n2o_co2_emissions_saved(),
+        soln_pds_direct_co2eq_emissions_saved=self.ua.direct_co2eq_emissions_saved_land(),
+        soln_pds_direct_co2_emissions_saved=self.ua.direct_co2_emissions_saved_land(),
+        soln_pds_direct_n2o_co2_emissions_saved=self.ua.direct_n2o_co2_emissions_saved_land(),
+        soln_pds_direct_ch4_co2_emissions_saved=self.ua.direct_ch4_co2_emissions_saved_land(),
         soln_pds_new_iunits_reqd=self.ua.soln_pds_new_iunits_reqd(),
         soln_ref_new_iunits_reqd=self.ua.soln_ref_new_iunits_reqd(),
         conv_ref_new_iunits=self.ua.conv_ref_new_iunits(),
         conv_ref_grid_CO2_per_KWh=self.ef.conv_ref_grid_CO2_per_KWh(),
         conv_ref_grid_CO2eq_per_KWh=self.ef.conv_ref_grid_CO2eq_per_KWh(),
         soln_net_annual_funits_adopted=soln_net_annual_funits_adopted,
-        avoided_direct_emissions=self.ua.direct_co2eq_emissions_saved_land(),
         land_distribution=self.ae.get_land_distribution())
 
