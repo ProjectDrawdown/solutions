bff079a3182142dbeafdd12c9ee178f9b40a2481
Denton Gentry <dgentry@carboncaptu.re>
2019-05-15 05:52:27 -0700

    solution/factory: add temperateforests
    temperateforests had been missed and not added to solution/factory.py.
    
    Also add a test case to check for this.

--- solution/test_factory.py
+++ solution/test_factory.py
@@ -1,5 +1,7 @@
 """Test solution classes."""
 
+import os
+import pandas as pd
 import pytest
 import factory
 
@@ -7,3 +9,11 @@ def test_all_solutions_scenarios():
     result = factory.all_solutions_scenarios()
     assert 'solarpvutil' in result.keys()
     assert 'silvopasture' in result.keys()
+
+def test_all_solutions_present():
+    all_solutions = pd.read_csv(os.path.join('data', 'overview', 'solutions.csv'),
+            index_col=False, skipinitialspace=True, header=0,
+            skip_blank_lines=True, comment='#')
+    factory_solutions = factory.all_solutions_scenarios()
+    for (_, val) in all_solutions['DirName'].dropna().iteritems():
+        assert val in factory_solutions.keys()
