d16e2a75edbbb2cdeea34f8f8bb9feec67c4ff5f
Denton Gentry <dgentry@carboncaptu.re>
2020-01-20 07:44:41 -0800

    firstcost: remove regional costing and tests.
    firstcost added support for computing cost per region some time
    ago. This is a reasonable idea, but it is not used by any of the
    solution models and is incomplete as only the first cost supports
    it.
    
    This should be done in the future when we're ready to use it, and
    done for keepsies across all solutions not by adding conditionals
    throughout the implementation.

--- model/firstcost.py
+++ model/firstcost.py
@@ -5,8 +5,7 @@ import math
 import numpy as np
 import pandas as pd
 
-from model.advanced_controls import fill_missing_regions_from_world
-from model.dd import REGIONS, MAIN_REGIONS
+import model.dd
 
 
 class FirstCost:
@@ -43,8 +42,7 @@ class FirstCost:
                  ref_learning_increase_mult, conv_learning_increase_mult,
                  soln_pds_tot_iunits_reqd, soln_ref_tot_iunits_reqd, conv_ref_tot_iunits,
                  soln_pds_new_iunits_reqd, soln_ref_new_iunits_reqd, conv_ref_new_iunits,
-                 fc_convert_iunit_factor=1.0, conv_ref_first_cost_uses_tot_units=False,
-                 calc_per_region=False):
+                 fc_convert_iunit_factor=1.0, conv_ref_first_cost_uses_tot_units=False):
         self.ac = ac
         self.pds_learning_increase_mult = pds_learning_increase_mult
         self.ref_learning_increase_mult = ref_learning_increase_mult
@@ -57,7 +55,6 @@ class FirstCost:
         self.conv_ref_new_iunits = conv_ref_new_iunits
         self.fc_convert_iunit_factor = fc_convert_iunit_factor
         self.conv_ref_first_cost_uses_tot_units = conv_ref_first_cost_uses_tot_units
-        self.calc_per_region = calc_per_region
 
     @lru_cache()
     def soln_pds_install_cost_per_iunit(self):
@@ -68,22 +65,24 @@ class FirstCost:
         log_learning_mult = math.log10(self.pds_learning_increase_mult)
         parameter_b = log_learning_rate / log_learning_mult
 
-        regions = REGIONS if self.calc_per_region else ['World']
+        main_region = model.dd.REGIONS[0]
         if 2015 in self.soln_pds_tot_iunits_reqd.index:
-            base_year_iunits_reqd = self.soln_pds_tot_iunits_reqd.loc[2015, regions]
+            base_year_iunits_reqd = self.soln_pds_tot_iunits_reqd.loc[2015, main_region]
         elif 2018 in self.soln_pds_tot_iunits_reqd.index:
-            base_year_iunits_reqd = self.soln_pds_tot_iunits_reqd.loc[2018, regions]
+            base_year_iunits_reqd = self.soln_pds_tot_iunits_reqd.loc[2018, main_region]
         else:
             raise ValueError('No base year found in soln_pds_tot_iunits_reqd')
-        p = (1 / base_year_iunits_reqd.replace(0, 1)) ** parameter_b
-        first_unit_cost = fill_missing_regions_from_world(self.ac.pds_2014_cost) * p
+        if base_year_iunits_reqd == 0:
+            first_unit_cost = self.ac.pds_2014_cost
+        else:
+            p = (1 / base_year_iunits_reqd) ** parameter_b
+            first_unit_cost = self.ac.pds_2014_cost * p
 
-        iunits = self.soln_pds_tot_iunits_reqd.loc[:, regions]
-        result_per_iunit = first_unit_cost * iunits.replace(0, 1) ** parameter_b
+        iunits = self.soln_pds_tot_iunits_reqd.loc[:, main_region]
+        result_per_iunit = first_unit_cost * iunits ** parameter_b
 
         # In Excel, NaN^0 == NaN. In Python, NaN^0 == 1.
-        # We want to match the Excel behavior.
-        # See First Cost Handling of NaN data in:
+        # We want to match the Excel behavior.  See First Cost Handling of NaN data in:
         # https://docs.google.com/document/d/19sq88J_PXY-y_EnqbSJDl0v9CdJArOdFLatNNUFhjEA/edit#
         result_per_iunit.mask(iunits.isna(), other=np.nan, inplace=True)
         result_per_iunit.mask(iunits == 0.0, other=np.nan, inplace=True)
@@ -93,16 +92,8 @@ class FirstCost:
             result = result_display
         else:
             conv = self.conv_ref_install_cost_per_iunit()
-            if type(conv) == pd.Series:  # convert series back to df for combine function.
-                conv = conv.to_frame().rename({'conv_ref_install_cost_per_iunit': 'World'}, axis='columns')
             result = result_display.combine(conv, np.maximum)
 
-        if not self.calc_per_region:  # convert to series
-            result = result['World']
-        else:
-            # world costs will be summed from regions when combined with adoption data
-            # the world costs calculated here are thus misleading, so we replace with NaN
-            result.loc[:, 'World'] = np.nan
         result.name = "soln_pds_install_cost_per_iunit"
         return result
 
@@ -114,17 +105,26 @@ class FirstCost:
         log_learning_rate = math.log10(self.ac.conv_first_cost_learning_rate)
         log_learning_mult = math.log10(self.conv_learning_increase_mult)
         parameter_b = log_learning_rate / log_learning_mult
-        regions = REGIONS if self.calc_per_region else ['World']
+        main_region = model.dd.REGIONS[0]
 
-        # Excel implementation referenced the cell for 2014, not 2015, so we
-        # do the same here. Normally, we base calculations from 2015.
-        p = (1 / self.conv_ref_tot_iunits.loc[2014, regions].replace(0, 1)) ** parameter_b
-        first_unit_cost = fill_missing_regions_from_world(self.ac.conv_2014_cost) * p
+        if 2014 in self.conv_ref_tot_iunits.index:
+            # Excel implementation referenced the cell for 2014, not 2015, so we
+            # do the same here. Normally, we base calculations from 2015.
+            base_year_iunits_reqd = self.conv_ref_tot_iunits.loc[2014, main_region]
+        elif 2018 in self.conv_ref_tot_iunits.index:
+            base_year_iunits_reqd = self.conv_ref_tot_iunits.loc[2018, main_region]
+        else:
+            raise ValueError('No base year found in conv_ref_tot_iunits')
+        if base_year_iunits_reqd == 0:
+            first_unit_cost = self.ac.conv_2014_cost
+        else:
+            p = (1 / base_year_iunits_reqd) ** parameter_b
+            first_unit_cost = self.ac.conv_2014_cost * p
 
-        iunits = self.conv_ref_tot_iunits.loc[:, regions]
+        iunits = self.conv_ref_tot_iunits.loc[:, main_region].copy()
         if parameter_b == 0:  # required to pass integration tests
-            iunits.loc[:, :] = 1
-        result_per_iunit = first_unit_cost * iunits.replace(0, 1) ** parameter_b
+            iunits.loc[:] = 1
+        result_per_iunit = first_unit_cost * iunits ** parameter_b
 
         # In Excel, NaN^0 == NaN. In Python, NaN^0 == 1.
         # We want to match the Excel behavior.
@@ -138,14 +138,8 @@ class FirstCost:
         # through the array comparing each year to the previous year.
         step2 = step1.rolling(2).apply(lambda x: min(x[0], x[1]), raw=True)
         first = step1.first_valid_index()
-        step2.loc[first, :] = step1.loc[first, :]  # no min() for first item
+        step2.loc[first] = step1.loc[first]  # no min() for first item
 
-        if not self.calc_per_region:  # convert to series
-            step2 = step2['World']
-        else:
-            # world costs will be summed from regions when combined with adoption data
-            # the world costs calculated here are thus misleading, so we replace with NaN
-            step2.loc[:, 'World'] = np.nan
         step2.name = "conv_ref_install_cost_per_iunit"
         return step2
 
@@ -158,20 +152,23 @@ class FirstCost:
         log_learning_mult = math.log10(self.ref_learning_increase_mult)
         parameter_b = log_learning_rate / log_learning_mult
 
-        regions = REGIONS if self.calc_per_region else ['World']
+        main_region = model.dd.REGIONS[0]
         if 2015 in self.soln_ref_tot_iunits_reqd.index:
-            base_year_iunits_reqd = self.soln_ref_tot_iunits_reqd.loc[2015, regions]
+            base_year_iunits_reqd = self.soln_ref_tot_iunits_reqd.loc[2015, main_region]
         elif 2018 in self.soln_ref_tot_iunits_reqd.index:
-            base_year_iunits_reqd = self.soln_ref_tot_iunits_reqd.loc[2018, regions]
+            base_year_iunits_reqd = self.soln_ref_tot_iunits_reqd.loc[2018, main_region]
         else:
             raise ValueError('No base year found in soln_ref_tot_iunits_reqd')
-        p = (1 / base_year_iunits_reqd.replace(0, 1)) ** parameter_b
-        first_unit_cost = fill_missing_regions_from_world(self.ac.ref_2014_cost) * p
+        if base_year_iunits_reqd == 0:
+            first_unit_cost = self.ac.ref_2014_cost
+        else:
+            p = (1 / base_year_iunits_reqd) ** parameter_b
+            first_unit_cost = self.ac.ref_2014_cost * p
 
-        iunits = self.soln_ref_tot_iunits_reqd.loc[:, regions]
+        iunits = self.soln_ref_tot_iunits_reqd.loc[:, main_region].copy()
         if parameter_b == 0:  # required to pass integration tests
-            iunits.loc[:, :] = 1
-        result_per_iunit = first_unit_cost * iunits.replace(0, 1) ** parameter_b
+            iunits.loc[:] = 1
+        result_per_iunit = first_unit_cost * iunits ** parameter_b
 
         # In Excel, NaN^0 == NaN. In Python, NaN^0 == 1.
         # We want to match the Excel behavior.
@@ -184,16 +181,8 @@ class FirstCost:
             result = step1
         else:
             conv = self.conv_ref_install_cost_per_iunit()
-            if type(conv) == pd.Series:  # convert series back to df for combine function.
-                conv = conv.to_frame().rename({'conv_ref_install_cost_per_iunit': 'World'}, axis='columns')
             result = step1.combine(conv, np.maximum)
 
-        if not self.calc_per_region:  # convert to series
-            result = result['World']
-        else:
-            # world costs will be summed from regions when combined with adoption data
-            # the world costs calculated here are thus misleading, so we replace with NaN
-            result.loc[:, 'World'] = np.nan
         result.name = "soln_ref_install_cost_per_iunit"
         return result
 
@@ -202,11 +191,8 @@ class FirstCost:
         """Annual World First Cost (SOLUTION-PDS)
            'First Cost'!E37:E82
         """
-        if self.calc_per_region:
-            result = (self.soln_pds_new_iunits_reqd.loc[:, MAIN_REGIONS] *
-                      self.soln_pds_install_cost_per_iunit().loc[:, MAIN_REGIONS]).sum(axis=1)
-        else:
-            result = self.soln_pds_new_iunits_reqd["World"] * self.soln_pds_install_cost_per_iunit()
+        main_region = model.dd.REGIONS[0]
+        result = self.soln_pds_new_iunits_reqd[main_region] * self.soln_pds_install_cost_per_iunit()
         result.name = "soln_pds_annual_world_first_cost"
         return result
 
@@ -215,11 +201,8 @@ class FirstCost:
         """Annual World First Cost (SOLUTION-REF)
            'First Cost'!N37:N82
         """
-        if self.calc_per_region:
-            result = (self.soln_ref_new_iunits_reqd.loc[:, MAIN_REGIONS] *
-                      self.soln_ref_install_cost_per_iunit().loc[:, MAIN_REGIONS]).sum(axis=1)
-        else:
-            result = self.soln_ref_new_iunits_reqd["World"] * self.soln_ref_install_cost_per_iunit()
+        main_region = model.dd.REGIONS[0]
+        result = self.soln_ref_new_iunits_reqd[main_region] * self.soln_ref_install_cost_per_iunit()
         result.name = "soln_ref_annual_world_first_cost"
         return result
 
@@ -228,16 +211,13 @@ class FirstCost:
         """Annual World First Cost (CONVENTIONAL-REF)
            'First Cost'!Q37:Q82
         """
+        main_region = model.dd.REGIONS[0]
         if self.conv_ref_first_cost_uses_tot_units:
             iunits = self.conv_ref_tot_iunits.diff()
         else:
             iunits = self.conv_ref_new_iunits
 
-        if self.calc_per_region:
-            result = (iunits.loc[:, MAIN_REGIONS] *
-                      self.conv_ref_install_cost_per_iunit().loc[:, MAIN_REGIONS]).sum(axis=1)
-        else:
-            result = iunits['World'] * self.conv_ref_install_cost_per_iunit()
+        result = iunits[main_region] * self.conv_ref_install_cost_per_iunit()
         result.name = "conv_ref_annual_world_first_cost"
         return result
 
