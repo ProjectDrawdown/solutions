6c7505a6eb4476b6c373fd6baf9b65d225c0dabd
Denton Gentry <dgentry@carboncaptu.re>
2020-02-16 09:15:01 -0800

    excel_fair_results: split across sectors
    A number of the solutions are sorted into multiple sectors.
    When this happens, split their emissions reduction equally.

--- tools/excel_fair_results.py
+++ tools/excel_fair_results.py
@@ -14,157 +14,133 @@ import pandas as pd
 
 g_years = range(1850, 2061)
 animation_sectors = {
-    'Electricity': 'Electricity',
-    'Smart Thermostats': 'Electricity',
-    'Building Automation Systems': 'Electricity',
-    'LED Lighting': 'Electricity',
-    'LED Lighting (commercial)': 'Electricity',
-    'LED Lighting (residential)': 'Electricity',
-    'Insulation': 'Electricity',
-    'Dynamic Glass': 'Electricity',
-    'High-Performance Glass': 'Electricity',
-    'High-Performance Glass (commercial)': 'Electricity',
-    'Green & Cool Roofs': 'Electricity',
-    'District Heating': 'Electricity',
-    'High-Efficiency Heat Pumps': 'Electricity',
-    'High-Efficient Heat Pumps': 'Electricity',
-    'Efficient Cooling Devices': 'Electricity',
-    'Solar Hot Water': 'Electricity',
-    'Low-Flow Fixtures': 'Electricity',
-    'Water Distribution Efficiency': 'Electricity',
-    'Building Retrofitting': 'Electricity',
-    'Net-Zero Buildings': 'Electricity',
-    'Concentrated Solar Power': 'Electricity',
-    'Distributed Solar Photovoltaics': 'Electricity',
-    'Utility-Scale Solar Photovoltaics': 'Electricity',
-    'Utility-scale Solar Photovoltaics': 'Electricity',
-    'Micro WInd Turbines': 'Electricity',
-    'Micro Wind Turbines': 'Electricity',
-    'Onshore Wind Turbines': 'Electricity',
-    'Offshore WInd Turbines': 'Electricity',
-    'Offshore Wind Turbines': 'Electricity',
-    'Geothermal Power': 'Electricity',
-    'Small Hydropower': 'Electricity',
-    'Ocean Power': 'Electricity',
-    'Biomass Power': 'Electricity',
-    'Nuclear Power': 'Electricity',
-    'Waste-to-Energy': 'Electricity',
-    'Waste to Energy': 'Electricity',
-    'Landfill Methane Capture': 'Electricity',
-    'Methane Digesters': 'Electricity',
-    'Large Methane Digesters': 'Electricity',
-    'Grid Flexibility': 'Electricity',
-    'Microgrids': 'Electricity',
-    'Distributed Energy Storage': 'Electricity',
-    'Utility-Scale Energy Storage': 'Electricity',
-    'Plant-Rich Diets':  'Food, Agriculture, Land Use',
-    'Plant-Rich Diet':  'Food, Agriculture, Land Use',
-    'Reduced Food Waste':  'Food, Agriculture, Land Use',
-    'Macro Algal Farming':  'Food, Agriculture, Land Use',
-    'Avoided Land Conversion':  'Food, Agriculture, Land Use',
-    'Avoided Land Use Conversion':  'Food, Agriculture, Land Use',
-    'Forest Protection':  'Food, Agriculture, Land Use',
-    "Indigenous Peoples' Forest Tenure":  'Food, Agriculture, Land Use',
-    'Sustainable Forestry':  'Food, Agriculture, Land Use',
-    'Grassland Protection':  'Food, Agriculture, Land Use',
-    'Peatland Protection':  'Food, Agriculture, Land Use',
-    'Peatland Protection & Rewetting': 'Food, Agriculture, Land Use',
-    'Coastal Wetland Protection':  'Food, Agriculture, Land Use',
-    'Sustainable Intensification for Smallholders':  'Food, Agriculture, Land Use',
-    'Conservation Agriculture':  'Food, Agriculture, Land Use',
-    'Regenerative Annual Cropping':  'Food, Agriculture, Land Use',
-    'Improved Livestock Feeds':  'Food, Agriculture, Land Use',
-    'Nutrient Management':  'Food, Agriculture, Land Use',
-    'Farm Irrigation Efficiency':  'Food, Agriculture, Land Use',
-    'Improved Rice Production':  'Food, Agriculture, Land Use',
-    'System of Rice Intensification':  'Food, Agriculture, Land Use',
-    'System of Rice Instensification':  'Food, Agriculture, Land Use',
-    'TBD': 'Industry',
-    'Alternative Cement': 'Industry',
-    'Alternative Concrete': 'Industry',
-    'Alternative Cements': 'Industry',
-    'Bioplastics': 'Industry',
-    'Refrigerant Management': 'Industry',
-    'Alternative Refrigerants': 'Industry',
-    'Composting': 'Industry',
-    'Recycling': 'Industry',
-    'Recycled Paper': 'Industry',
-    'Waste-to-Energy': 'Industry',
-    'Landfill Methane Capture': 'Industry',
-    'Methane Digesters': 'Industry',
-    'Hybrid Cars': 'Transportation',
-    'Efficient Trucks': 'Transportation',
-    'Efficient Aviation': 'Transportation',
-    'Efficient Ocean Shipping': 'Transportation',
-    'Walkable Cities': 'Transportation',
-    'Bicycle Infrastructure': 'Transportation',
-    'Bike Infrastructure': 'Transportation',
-    'Electric Bicycles': 'Transportation',
-    'Carpooling': 'Transportation',
-    'Public Transit': 'Transportation',
-    'High-Speed Rail': 'Transportation',
-    'Telepresence': 'Transportation',
-    'Electric Cars': 'Transportation',
-    'Electric Trains': 'Transportation',
-    'Smart Thermostats': 'Buildings',
-    'Building Automation Systems': 'Buildings',
-    'Insulation': 'Buildings',
-    'Dynamic Glass': 'Buildings',
-    'High-Performance Glass': 'Buildings',
-    'Green & Cool Roofs': 'Buildings',
-    'Green & Cool Roofs ': 'Buildings',
-    'Green Roofs': 'Buildings',
-    'Cool Roofs': 'Buildings',
-    'Low-Flow Fixtures': 'Buildings',
-    'Building Retrofitting': 'Buildings',
-    'Net-Zero Buildings': 'Buildings',
-    'District Heating': 'Buildings',
-    'High-Efficiency Heat Pumps': 'Buildings',
-    'Solar Hot Water': 'Buildings',
-    'Biogas for Cooking': 'Buildings',
-    'Improved Clean Cookstoves': 'Buildings',
-    'Refrigerant Management': 'Buildings',
-    'Alternative Refrigerants': 'Buildings',
-    'N/A':  'Other',
-    'Plant-Rich Diets': 'Land Sinks',
-    'Reduced Food Waste': 'Land Sinks',
-    'Avoided Land Conversion': 'Land Sinks',
-    'Forest Protection': 'Land Sinks',
-    'Indigenous Peoples Forest Tenure': 'Land Sinks',
-    'Sustainable Forestry': 'Land Sinks',
-    'Temperate Forest Restoration': 'Land Sinks',
-    'Tropical Forest Restoration': 'Land Sinks',
-    'Peatland Protection': 'Land Sinks',
-    'Peatland Restoration': 'Land Sinks',
-    'Sustainable Intensification for Smallholders': 'Land Sinks',
-    'Conservation Agriculture': 'Land Sinks',
-    'Regenerative Annual Cropping': 'Land Sinks',
-    'Managed Grazing': 'Land Sinks',
-    'Silvopasture': 'Land Sinks',
-    'Multistrata Agroforestry': 'Land Sinks',
-    'Tree Intercropping': 'Land Sinks',
-    'Perennial Staple Crops': 'Land Sinks',
-    'Perennial Biomass Production': 'Land Sinks',
-    'Improved Rice Cultivation': 'Land Sinks',
-    'System of Rice Intensification': 'Land Sinks',
-    'Abandoned Farmland Restoration': 'Land Sinks',
-    'Tree Plantations (on Degraded Land)': 'Land Sinks',
-    'Tree Plantations on Degraded Land': 'Land Sinks',
-    'Bamboo Production': 'Land Sinks',
-    'Coastal Wetland Protection':  'Coastal & Ocean Sinks',
-    'Coastal Wetland Restoration':  'Coastal & Ocean Sinks',
-    'Coastal Wetlands Restoration': 'Coastal & Ocean Sinks',
-    'Marine Protected Areas':  'Coastal & Ocean Sinks',
-    'Kelp Forest Restoration':  'Coastal & Ocean Sinks',
-    'Macro Algal Farming':  'Coastal & Ocean Sinks',
-    'Marine Permaculture':  'Coastal & Ocean Sinks',
-    'Ocean Farming':  'Coastal & Ocean Sinks',
-    'Biochar Production':  'Engineered Sinks',
-    'Other coming attractions *':  'Engineered Sinks',
-    'Health & Education': 'Health & Education',
-    'Health and Education': 'Health & Education',
-    'Educating Girls': 'Health & Education',
-    'Family Planning': 'Health & Education',
+    'Electricity': ['Electricity'],
+    'Cogeneration': ['Electricity'],
+    'Smart Thermostats': ['Electricity', 'Buildings'],
+    'Building Automation Systems': ['Electricity', 'Buildings'],
+    'LED Lighting': ['Electricity'],
+    'LED Lighting (commercial)': ['Electricity'],
+    'LED Lighting (residential)': ['Electricity'],
+    'Insulation': ['Electricity', 'Buildings'],
+    'Dynamic Glass': ['Electricity', 'Buildings'],
+    'High-Performance Glass': ['Electricity', 'Buildings'],
+    'High-Performance Glass (commercial)': ['Electricity', 'Buildings'],
+    'High-Performance Glass (residential)': ['Electricity', 'Buildings'],
+    'Green & Cool Roofs': ['Electricity', 'Buildings'],
+    'Green Roofs': ['Electricity', 'Buildings'],
+    'Cool Roofs': ['Electricity', 'Buildings'],
+    'District Heating': ['Electricity', 'Buildings'],
+    'High-Efficiency Heat Pumps': ['Electricity', 'Buildings'],
+    'High-Efficient Heat Pumps': ['Electricity', 'Buildings'],
+    'Efficient Cooling Devices': ['Electricity'],
+    'Solar Hot Water': ['Electricity', 'Buildings'],
+    'Low-Flow Fixtures': ['Electricity', 'Buildings'],
+    'Water Distribution Efficiency': ['Electricity'],
+    'Building Retrofitting': ['Electricity', 'Buildings'],
+    'Net-Zero Buildings': ['Electricity', 'Buildings'],
+    'Concentrated Solar Power': ['Electricity'],
+    'Distributed Solar Photovoltaics': ['Electricity'],
+    'Utility-Scale Solar Photovoltaics': ['Electricity'],
+    'Utility-scale Solar Photovoltaics': ['Electricity'],
+    'Micro WInd Turbines': ['Electricity'],
+    'Micro Wind Turbines': ['Electricity'],
+    'Onshore Wind Turbines': ['Electricity'],
+    'Offshore WInd Turbines': ['Electricity'],
+    'Offshore Wind Turbines': ['Electricity'],
+    'Geothermal Power': ['Electricity'],
+    'Small Hydropower': ['Electricity'],
+    'Ocean Power': ['Electricity'],
+    'Biomass Power': ['Electricity'],
+    'Nuclear Power': ['Electricity'],
+    'Waste-to-Energy': ['Electricity', 'Industry'],
+    'Waste to Energy': ['Electricity', 'Industry'],
+    'Landfill Methane Capture': ['Electricity', 'Industry'],
+    'Methane Digesters': ['Electricity', 'Industry'],
+    'Large Methane Digesters': ['Electricity', 'Industry'],
+    'Grid Flexibility': ['Electricity'],
+    'Microgrids': ['Electricity'],
+    'Micro-Grids': ['Electricity'],
+    'Distributed Energy Storage': ['Electricity'],
+    'Utility-Scale Energy Storage': ['Electricity'],
+    'Plant-Rich Diets': [ 'Food, Agriculture, Land Use', 'Land Sinks'],
+    'Plant-Rich Diet': [ 'Food, Agriculture, Land Use', 'Land Sinks'],
+    'Reduced Food Waste': [ 'Food, Agriculture, Land Use', 'Land Sinks'],
+    'Macro Algal Farming': [ 'Food, Agriculture, Land Use', 'Coastal & Ocean Sinks'],
+    'Avoided Land Conversion': [ 'Food, Agriculture, Land Use', 'Land Sinks'],
+    'Avoided Land Use Conversion': [ 'Food, Agriculture, Land Use', 'Land Sinks'],
+    'Forest Protection': ['Food, Agriculture, Land Use', 'Land Sinks'],
+    "Indigenous Peoples' Forest Tenure": ['Food, Agriculture, Land Use'],
+    'Sustainable Forestry': ['Food, Agriculture, Land Use', 'Land Sinks'],
+    'Grassland Protection': ['Food, Agriculture, Land Use'],
+    'Peatland Protection': ['Food, Agriculture, Land Use', 'Land Sinks'],
+    'Peatland Protection & Rewetting': ['Food, Agriculture, Land Use', 'Land Sinks'],
+    'Peatland Restoration': ['Food, Agriculture, Land Use', 'Land Sinks'],
+    'Coastal Wetland Protection': ['Food, Agriculture, Land Use', 'Coastal & Ocean Sinks'],
+    'Coastal Wetland Protection': ['Food, Agriculture, Land Use', 'Coastal & Ocean Sinks'],
+    'Coastal Wetland Restoration': ['Food, Agriculture, Land Use', 'Coastal & Ocean Sinks'],
+    'Coastal Wetlands Restoration': ['Food, Agriculture, Land Use', 'Coastal & Ocean Sinks'],
+    'Sustainable Intensification for Smallholders': ['Food, Agriculture, Land Use', 'Land Sinks'],
+    'Conservation Agriculture': [ 'Food, Agriculture, Land Use', 'Land Sinks'],
+    'Regenerative Annual Cropping': [ 'Food, Agriculture, Land Use', 'Land Sinks'],
+    'Improved Livestock Feeds': [ 'Food, Agriculture, Land Use'],
+    'Nutrient Management': [ 'Food, Agriculture, Land Use'],
+    'Farm Irrigation Efficiency': [ 'Food, Agriculture, Land Use'],
+    'Improved Rice Production': [ 'Food, Agriculture, Land Use'],
+    'System of Rice Intensification': [ 'Food, Agriculture, Land Use', 'Land Sinks'],
+    'System of Rice Instensification': [ 'Food, Agriculture, Land Use', 'Land Sinks'],
+    'TBD': ['Industry'],
+    'Alternative Cement': ['Industry'],
+    'Alternative Concrete': ['Industry'],
+    'Alternative Cements': ['Industry'],
+    'Bioplastics': ['Industry'],
+    'Refrigerant Management': ['Industry', 'Buildings'],
+    'Alternative Refrigerants': ['Industry', 'Buildings'],
+    'Composting': ['Industry'],
+    'Recycling': ['Industry'],
+    'Recycling (commercial)': ['Industry'],
+    'Recycling (residential)': ['Industry'],
+    'Recycled Paper': ['Industry'],
+    'Hybrid Cars': ['Transportation'],
+    'Efficient Trucks': ['Transportation'],
+    'Efficient Aviation': ['Transportation'],
+    'Efficient Ocean Shipping': ['Transportation'],
+    'Walkable Cities': ['Transportation'],
+    'Bicycle Infrastructure': ['Transportation'],
+    'Bike Infrastructure': ['Transportation'],
+    'Electric Bicycles': ['Transportation'],
+    'Carpooling': ['Transportation'],
+    'Public Transit': ['Transportation'],
+    'High-Speed Rail': ['Transportation'],
+    'Telepresence': ['Transportation'],
+    'Electric Cars': ['Transportation'],
+    'Electric Trains': ['Transportation'],
+    'Biogas for Cooking': ['Buildings'],
+    'Improved Clean Cookstoves': ['Buildings'],
+    'N/A': [ 'Other'],
+    'Indigenous Peoples Forest Tenure': ['Land Sinks'],
+    'Temperate Forest Restoration': ['Land Sinks'],
+    'Tropical Forest Restoration': ['Land Sinks'],
+    'Managed Grazing': ['Land Sinks'],
+    'Silvopasture': ['Land Sinks'],
+    'Multistrata Agroforestry': ['Land Sinks'],
+    'Tree Intercropping': ['Land Sinks'],
+    'Perennial Staple Crops': ['Land Sinks'],
+    'Perennial Biomass Production': ['Land Sinks'],
+    'Improved Rice Cultivation': ['Land Sinks'],
+    'Abandoned Farmland Restoration': ['Land Sinks'],
+    'Tree Plantations (on Degraded Land)': ['Land Sinks'],
+    'Tree Plantations on Degraded Land': ['Land Sinks'],
+    'Bamboo Production': ['Land Sinks'],
+    'Marine Protected Areas': [ 'Coastal & Ocean Sinks'],
+    'Kelp Forest Restoration': [ 'Coastal & Ocean Sinks'],
+    'Marine Permaculture': [ 'Coastal & Ocean Sinks'],
+    'Ocean Farming': [ 'Coastal & Ocean Sinks'],
+    'Biochar Production': [ 'Engineered Sinks'],
+    'Other coming attractions *': [ 'Engineered Sinks'],
+    'Health & Education': ['Health & Education'],
+    'Health and Education': ['Health & Education'],
+    'Educating Girls': ['Health & Education'],
+    'Family Planning': ['Health & Education'],
 }
 
 colors = {
@@ -187,31 +163,27 @@ def baseline(solutions):
 def process_scenario(filename, outdir, scenario):
     sheet_name = 'Gtperyr_' + scenario
     raw = pd.read_excel(io=filename, sheet_name=sheet_name, header=None, index_col=0,
-            dtype=object, skiprows=0, nrows=52, usecols='A:IL')
-    solution_names = sorted(list(set(raw.iloc[5, 3:].dropna())))
+            dtype=object, skiprows=0, nrows=52, usecols='A:HR')
+    solution_names = sorted([x.strip(' ') for x in list(set(raw.iloc[5, 9:].dropna()))])
 
     solutions = pd.DataFrame(0, index=g_years, columns=solution_names)
     solutions.index.name = 'Year'
-    sectors = {}
     prev_solution = None
-    for (_, col) in raw.iloc[:, 3:].iteritems():
+    for (_, col) in raw.iloc[:, 9:].iteritems():
         numeric = pd.to_numeric(col.iloc[11:], errors='coerce').fillna(0.0)
         if np.count_nonzero(numeric.to_numpy()) == 0:
             continue
         disposition = col.iloc[3]
-        if disposition not in ['Avoided', 'Sequestration', 'ELC', 'F+D', 'INDIRECT']:
+        if disposition not in ['Avoided', 'Sequestration', 'ELC', 'F+D', 'IND']:
             continue
         mechanism = col.iloc[3] if not pd.isna(col.iloc[3]) else 'Avoided'
-        solution = col.iloc[5] if not pd.isna(col.iloc[5]) else prev_solution
+        solution = col.iloc[5].strip() if not pd.isna(col.iloc[5]) else prev_solution
         if solution == 'Peatland Protection' or solution == 'Peatland Restoration':
             continue
-        sector = animation_sectors[solution]
         if mechanism == 'Avoided':
             numeric.iloc[0] = 0.0
         numeric.name = solution
         solutions[solution] += ((numeric / 1000.0) / 3.664)  # Mtons CO2 -> Gtons C
-        sector_list = sectors.get(sector, [])
-        sectors[sector] = sector_list + [solution]
         prev_solution = solution
 
     total = baseline(solutions)
@@ -229,6 +201,8 @@ def process_scenario(filename, outdir, scenario):
     concentration["Baseline (ppm)"] = baseline_C
     total = baseline(solutions)
     emissions = solutions.sum(axis=1)
+    #emissions = raw.iloc[11:, 0:2].sum(axis=1) / 3.664
+    #print(emissions.loc[2020:] * 3.664)
     total = total.subtract(emissions.fillna(0.0), fill_value=0.0)
     C,_,T = fair.forward.fair_scm(emissions=total.values, useMultigas=False,
                 r0=model.fairutil.r0, tcrecs=model.fairutil.tcrecs)
@@ -245,7 +219,7 @@ def process_scenario(filename, outdir, scenario):
     outfile = os.path.splitext(os.path.basename(filename))[0] + '_Concentration_' + scenario + '.csv'
     concentration.to_csv(os.path.join(outdir, outfile), float_format='%.3f')
 
-    return (solutions, sectors)
+    return solutions.dropna(axis='columns', how='all').fillna(0.0)
 
 
 def legend_no_duplicates(ax):
@@ -277,11 +251,15 @@ def animate(frame, ax, start, lines, emissions, baseline_T):
                 y2=df_T.loc[2020:end].values, color=color)
 
 
-def produce_animation(solutions, sectors, filename, writer):
+def produce_animation(solutions, filename, writer):
     sector_gtons = pd.DataFrame()
-    for sector, solution_list in sectors.items():
-        sector_gtons.loc[:, sector] = solutions.loc[:, solution_list].sum(axis=1)
-
+    for solution in solutions:
+        sectors = animation_sectors[solution]
+        for sector in sectors:
+            if sector not in sector_gtons.columns:
+                sector_gtons.loc[:, sector] = solutions.loc[:, solution] / len(sectors)
+            else:
+                sector_gtons.loc[:, sector] += solutions.loc[:, solution] / len(sectors)
     start = baseline(solutions)
     _,_,start_T = fair.forward.fair_scm(emissions=start.values, useMultigas=False,
             r0=model.fairutil.r0, tcrecs=model.fairutil.tcrecs)
@@ -301,7 +279,6 @@ def produce_animation(solutions, sectors, filename, writer):
         reduction_T += total_reduction_T * fraction.loc[2020:2061].fillna(0.0).values
         temperatures = start_T[255:296] - reduction_T
         df_T = pd.Series(temperatures, index=range(2020, 2061))
-        print(f"{sector}: {df_T}")
         emissions.append((sector, df_T))
 
     fig = plt.figure()
@@ -323,8 +300,7 @@ def process_ghgs(excelfile, outdir, writer=None, ext='.mp4'):
     matplotlib.style.use('ggplot')
     for scenario in ['PDS1', 'PDS2']:
         print(f"{scenario} CSV")
-        (solutions, sectors) = process_scenario(filename=excelfile, outdir=outdir,
-                scenario=scenario)
+        solutions = process_scenario(filename=excelfile, outdir=outdir, scenario=scenario)
         if writer is None:
             ffmpeg = matplotlib.animation.writers['ffmpeg']
             writer = ffmpeg(fps=15, bitrate=-1,
@@ -334,8 +310,7 @@ def process_ghgs(excelfile, outdir, writer=None, ext='.mp4'):
         if writer:
             print(f"{scenario} animation")
             mp4 = os.path.splitext(os.path.basename(excelfile))[0] + '_' + scenario + ext
-            produce_animation(solutions=solutions, sectors=sectors,
-                    filename=os.path.join(outdir, mp4), writer=writer)
+            produce_animation(solutions=solutions, filename=os.path.join(outdir, mp4), writer=writer)
 
 
 if __name__ == "__main__":
