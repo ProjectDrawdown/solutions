ed5a7e2b3326effd4741453aece8e1e293c3709b
avishayp <avishay.pinsky@gmail.com>
2020-02-27 01:20:00 +0200

    Fix pylint W0622: Redefining built-in  - also rephrased excel rounding to a more familiar form

--- model/excel_math.py
+++ model/excel_math.py
@@ -1,20 +1,15 @@
 import math
 
 
-
-def round(x):
-    is_neg = False;
-
-    # Excel rounds away from zero, so 1.5 rounds to 2 and -1.5 rounds to -2.
-    if x < 0.0:
-        is_neg = True
-        x = math.fabs(x)
-
-    # Determine the value rounded down, then add 0.5 to it. This won't be exactly 0.5
-    # due to floating point precision, but we're just going to compare the argument to it.
-    # If less than the halfway point, round down.
-    halfway = math.floor(x) + 0.5
-    x = math.floor(x) if x < halfway else math.ceil(x)
-    if is_neg:
-        x = -x
-    return int(x)
+def round_away(x):
+    """
+    excel rounds away from zero on breakeven
+    so 0.5 ==> 1.0, -0.5 ==> -1.0
+    python2 does the same, hence this code
+    copied almost verbatim from https://github.com/python/cpython/blob/master/Python/pymath.c#L71
+    """
+    absx = math.fabs(x)
+    y = math.floor(absx)
+    if absx - y >= 0.5:
+        y += 1.0
+    return int(math.copysign(y, x))
--- model/tests/test_excel_math.py
+++ model/tests/test_excel_math.py
@@ -5,16 +5,16 @@ from model import excel_math
 
 
 def test_round():
-    assert excel_math.round(1.4) == 1
-    assert excel_math.round(1.5) == 2
-    assert excel_math.round(1.6) == 2
-    assert excel_math.round(-1.4) == -1
-    assert excel_math.round(-1.5) == -2
-    assert excel_math.round(-1.6) == -2
+    assert excel_math.round_away(1.4) == 1
+    assert excel_math.round_away(1.5) == 2
+    assert excel_math.round_away(1.6) == 2
+    assert excel_math.round_away(-1.4) == -1
+    assert excel_math.round_away(-1.5) == -2
+    assert excel_math.round_away(-1.6) == -2
 
     # from OnshoreWind
-    assert excel_math.round(63998.595 / 2844.382) == 23
+    assert excel_math.round_away(63998.595 / 2844.382) == 23
 
     # from Water Efficiency
-    assert excel_math.round(1629.9388631986958 / 72.44172725327537) == 23
-    assert excel_math.round(1086.6259087991305 / 72.44172725327537) == 15
+    assert excel_math.round_away(1629.9388631986958 / 72.44172725327537) == 23
+    assert excel_math.round_away(1086.6259087991305 / 72.44172725327537) == 15
