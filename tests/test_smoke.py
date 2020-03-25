fb9130de29f862a53f86d1ab1039cac7644d14a7
Josh Preuss <josh.preuss@datarobot.com>
2020-03-25 09:28:30 -0400

    autodownload geckodriver

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,22 +1,22 @@
-import time
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from webdrivermanager import GeckoDriverManager
 
 
 @pytest.fixture(scope="session")
 def driver():
+    geckodriver_executable_path = GeckoDriverManager().download_and_install()[1]
     options = Options()
     options.headless = True
-    driver = webdriver.Firefox(executable_path='bin/geckodriver', options=options)
+    driver = webdriver.Firefox(executable_path=geckodriver_executable_path, options=options)
     yield driver
     driver.close()
 
 
 def test_voila_started(driver):
     driver.get("http://localhost:8866/")
-    time.sleep(10)
+    driver.implicitly_wait(20)
     assert "Drawdown" in driver.title
     elem = driver.find_element_by_xpath('//div[contains(text(), "Solution")]')
     assert elem
