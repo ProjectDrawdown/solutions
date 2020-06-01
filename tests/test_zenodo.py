21a0e4e8b8f936b68c4302243711adafa8358ce9
Sunishchal <sunishchaldev@gmail.com>
2020-06-01 01:08:49 -0700

    Modified test to read schema from local repo instead of URL

--- tests/test_zenodo.py
+++ tests/test_zenodo.py
@@ -3,14 +3,13 @@
 import json
 import pathlib
 import jsonschema
-import urllib.request
 
 thisdir = pathlib.Path(__file__).parents[0]
 parentdir = pathlib.Path(__file__).parents[1]
 
 
 def test_zenodo_json_schema():
-    with open(str(thisdir.joinpath('.zenodo_v1.schema.json'))) as s:
+    with open(str(thisdir.joinpath('zenodo_v1.schema.json'))) as s:
         schema = json.loads(s.read())
     with open(str(parentdir.joinpath('.zenodo.json'))) as z:
         instance = json.loads(z.read())
