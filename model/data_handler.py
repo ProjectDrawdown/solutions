1830eda42b618681179cd365fd894d00e33e7ed5
javm <quetzal1910@gmail.com>
2020-11-06 19:51:13 -0500

    Adding files

---
+++ model/data_handler.py
@@ -0,0 +1,22 @@
+""" Super class to contain and manage data """
+import pandas as pd
+import numpy as np
+
+class DataHandler:
+
+    def to_json(self):
+        var_keys = vars(self).keys()
+        outputs = dict()
+        obj_vars = dir(self)
+        obj_vars.remove('to_json')
+        for k in obj_vars:
+            func = getattr(self, k)
+            if( not k.startswith('__') and callable(func)):
+                outputs[k] = clean_nan(func())
+        return outputs
+
+def clean_nan(dataframe):
+    for year in dataframe.keys():
+        if (np.isnan(dataframe[year])):
+            dataframe[year] = 0.0
+    return dataframe
