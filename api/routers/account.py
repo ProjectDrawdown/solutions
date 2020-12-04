0dd17fa9de413a691aa8ceceba07757ab50e6324
javm <quetzal1910@gmail.com>
2020-12-04 14:32:42 -0500

    Adding endpoints for login with a specific provider to be used by the client app [PDD-46], [PDD-114]

--- api/routers/account.py
+++ api/routers/account.py
@@ -4,18 +4,25 @@ from api.config import get_settings
 from .schemas import Url, User, Token, AuthorizationResponse
 
 settings = get_settings()
-
-importname = 'api.routers.providers.' + settings.default_provider
-provider_module = importlib.import_module(importname)
-
 router = APIRouter()
+default_provider = settings.default_provider
 
-@router.get("/login")
-def get_login_url() -> Url:
+@router.get('/login')
+def get_login_url_default() -> Url:
+    return get_login_url(default_provider)
+
+@router.post('/authorize')
+async def verify_authorization_default(body: AuthorizationResponse) -> Token:
+    return await verify_authorization(body, default_provider)
+
+@router.get('/login/{provider}')
+def get_login_url(provider: str) -> Url:
+    importname = 'api.routers.providers.' + provider
+    provider_module = importlib.import_module(importname)
     return provider_module.login_url()
 
-@router.post("/authorize")
-async def verify_authorization(
- body: AuthorizationResponse
-) -> Token:
+@router.post('/authorize/{provider}')
+async def verify_authorization( body: AuthorizationResponse, provider: str) -> Token:
+    importname = 'api.routers.providers.' + provider
+    provider_module = importlib.import_module(importname)
     return await provider_module.exchange_code(body)
