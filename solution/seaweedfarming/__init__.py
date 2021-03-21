c9c43a60a072a41b1decf67147bfb624570a0f28
Alyssa-R <arivera@everquote.com>
2021-03-21 16:22:03 -0400

    seaweedfarming imports yield data frame:

--- solution/seaweedfarming/__init__.py
+++ solution/seaweedfarming/__init__.py
@@ -30,9 +30,11 @@ from solution import land
 
 # The time will come for these when I start importing things. Which will probably be like five minutes
 # DATADIR = str(pathlib.Path(__file__).parents[2].joinpath('data'))
-# THISDIR = pathlib.Path(__file__).parents[0]
+THISDIR = pathlib.Path(__file__).parents[0]
+YIELD_DATA_FILE = THISDIR.joinpath('OceanInputsDataFrame.csv')
 
-
+yield_df = pd.read_csv(YIELD_DATA_FILE)
+print(yield_df)
 # create a list of valid scenario objects
 # -- make csvs from excel model
 # -- each named scenario will: 
@@ -43,58 +45,10 @@ from solution import land
 
 # Note from actual implementation: 
 # > wanted to do 'PDS-5p2050-PDS custom 2020-Avg of All' but the year-by-year table was empty, skipped
-DEV_DEFAULT_SCENARIO_NAME = 'PDS-4p2050- Plausible PDS custom- Avg of All'
 
-scenarios = {
-    'PDS-4p2050- Plausible PDS custom- Avg of All': advanced_controls.AdvancedControls(
+scenarios = {'PDS-4p2050- Plausible PDS custom- Avg of All': advanced_controls.AdvancedControls()}
 
-        # general
-        solution_category=SOLUTION_CATEGORY.OCEAN,
-        # vmas=VMAs,
-        report_start_year=2019, report_end_year=2060, # TODO Started in 2019 because that's the first year that there is data
-        # TODO idea: why not have them start at year 0 and count up, so that the plan cannot become outdated in this way? 'within 5 lyears, xyz effects"
-         #Alyssa -- left off here Sat pm
 
-        # adoption
-        # soln_ref_adoption_regional_data=False, soln_pds_adoption_regional_data=False,
-        soln_pds_adoption_basis='Linear',
-        # soln_pds_adoption_custom_name='Average of All Custom Scenarios',
-        pds_adoption_final_percentage=[
-            ('World', 1.0), ('OECD90', 1.0), ('Eastern Europe', 1.0), ('Asia (Sans Japan)', 1.0),
-            ('Middle East and Africa', 1.0), ('Latin America', 1.0), ('ABNJ', 1.0),
-            ('China', 1.0), ('India', 1.0), ('EU', 1.0), ('USA', 1.0)],
-
-        # financial
-        pds_2014_cost=0.0, ref_2014_cost=0.0,
-        conv_2014_cost=0.0,
-        soln_first_cost_efficiency_rate=0.0,
-        conv_first_cost_efficiency_rate=0.0,
-        npv_discount_rate=0.1,
-        soln_expected_lifetime=30.0,
-        conv_expected_lifetime=30.0,
-        yield_from_conv_practice=0.0,
-        yield_gain_from_conv_to_soln=0.0,
-
-        soln_fixed_oper_cost_per_iunit=0.0,
-        conv_fixed_oper_cost_per_iunit=0.0,
-
-        # emissions
-        soln_indirect_co2_per_iunit=0.0,
-        conv_indirect_co2_per_unit=0.0,
-        soln_annual_energy_used=0.0, conv_annual_energy_used=0.0,
-
-        tco2eq_reduced_per_land_unit=5.0,
-        tco2eq_rplu_rate='One-time',
-
-        emissions_grid_source='Meta-Analysis', emissions_grid_range='Mean',
-        emissions_use_co2eq=True,
-        emissions_use_agg_co2eq=True,
-
-        # sequestration
-        seq_rate_global=0.,
-        disturbance_rate=0.02,
-    )
-}
 
 
 
