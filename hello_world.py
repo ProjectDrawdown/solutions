faf5f262b9812187c2095db5996b49681bd7f1a4
Denise Draper <draperd@acm.org>
2021-03-20 20:25:12 -0700

    Update hello_world.py
    Modified hello_world to run a single solution instead of all of them, for clarity and efficiency.

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,15 @@
-import solution.factory
-solutions = solution.factory.all_solutions_scenarios()
-obj = solutions["solarpvutil"][0](scenario='PDS-25p2050-Optimum2020')
-print( obj.c2.co2_mmt_reduced())
+import importlib
+
+# Load an instance of the solarvputil solution
+# This solution contains the code to compute all the results for all scenarios for this solution
+solution_name = "solarpvutil"
+solution = importlib.import_module('solution.'+solution_name)
+
+# To see those results, compute them for a specific scenario.
+# The scenarios supported for each solution are named in the 'ac' subdirectory of the solution, e.g.
+# look in solutions/solarpvutil/ac
+solution_data = solution.Scenario(scenario='PDS-25p2050-Optimum2020')
+
+# There are lots of different datasets inside the solution object.
+# You can see them in the __init__.py file for the solution you are working with.
+print(solution_data.c2.co2_mmt_reduced())
