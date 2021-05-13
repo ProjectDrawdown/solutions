3dfdc3ba27b94a0bdb53f97ed4c647c2c2ddbd0f
Denise Draper <draperd@acm.org>
2021-05-13 04:30:14 -0700

    improve plots in explorer
    Set some better defaults and added working tooltips.

--- ui/explorer.py
+++ ui/explorer.py
@@ -4,6 +4,27 @@ import bokeh.plotting as bp
 import bokeh.models as bm
 from bokeh.palettes import Dark2_8
 
+
+# Override these with your own functions if you want to do any of this differently.
+def plot_setup( ):
+    """Common initialization routine for bokeh plots"""
+    p = bp.figure(tools="pan,box_zoom,reset")
+    p.sizing_mode = "stretch_width"
+    p.max_width = 1000
+    p.yaxis.major_label_orientation = "vertical"
+    p.toolbar.logo = None
+    return p
+
+def hover_tool( points ):
+    """Return a bokeh HoverTool configured how we like it.  Return None to skip hovering entirely.
+    Parameter: points: the set of point renderers (used to make hover only show data over these points)"""
+    return bm.HoverTool(
+        renderers=points,
+        toggleable=False,
+        tooltips="""<div style='padding:10px'>$name @Year{0}: @$name{0,0.00}</div>"""
+    )
+
+
 class SolutionExporer:
     """Creates pretty charts for solution."""
     # The plan is that the charts will share some common selections, so you
@@ -22,9 +43,11 @@ class SolutionExporer:
         self.solution = solution
         self.scenario_count = len(self.solution.scenario_names)
 
-        # You can set this to any color sequence that you like.
-        self.colors = Dark2_8
-    
+        # The following parameters control some styling aspects of plotting.
+        # You can override them as you would like.
+        self.colors = Dark2_8   # Colors for lines are taken from this sequence
+        self.plot_setup = plot_setup
+        self.hover_tool = hover_tool
 
     # A note on the plotting strategy used here: 
     # Our pandas dataframe has (scenario x year) for the row index
@@ -35,16 +58,38 @@ class SolutionExporer:
     # each scenario draw a line for each region.
 
     def show_co2_reduction(self, keep_zeros=False):
-        p = bp.figure()
+
+        # get the data from the solution
         co2_r_data = self.solution.co2_reduction()
-        cols = co2_r_data.columns
+
+        # plot it
+        p = self.plot_setup()
+        p.title="CO2 Equiv Reduction"
+        p.yaxis.axis_label="Gigatons"
         colors = itertools.cycle(self.colors)
+
+        points = []
         for i in range(self.scenario_count):
-            sdata = co2_r_data.loc[i]
-            # ColumnDataSource tells bokeh how to format pandas data 
-            cds = bm.ColumnDataSource(sdata)
+
+            sdata = co2_r_data.loc[i]  # data for the i'th scenario
+            # One of those bits of code that makes things needlessly confusing....
+            # Bokeh has certain limitations in how it can interact with data that is provided via multiple 
+            # separate renderings, which we are doing below both with the circle and line, and over multiple 
+            # scenarios/dataframes.  To make it easy to use other Bokeh functionality (like hover tooltips)
+            # we re-name the individual scenario dataframe columns to embed the scenario number.
+            # This is really a secondary bit of magic; ignore it the first time you read the code.
+            sdata.columns = [ f's{i} - {c}' for c in sdata.columns ]
+
+            # Now give it to Bokeh
+            cds = bm.ColumnDataSource(sdata)    
             for c in sdata.columns:
                 if keep_zeros or np.count_nonzero(sdata[c]) > 0:
-                    p.line(x='Year', y=c, source=cds, legend_label=f's{i} - {c}', color=next(colors))
+                    color = next(colors)
+                    p.line(x='Year', y=c, source=cds, color=color, legend_label=c )
+                    points.append( p.circle(x='Year', y=c, source=cds, color=color, name=c, hit_dilation=2.5 ) )
+        
+        hv = self.hover_tool(points)
+        if hv: 
+            p.add_tools(hv)
         bp.show(p)
 
