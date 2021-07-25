3e46b045716db9b1f179f6b8dd6cd3f315a13eae
ssmssam <ssmssam@gmail.com>
2021-07-25 08:52:56 +0100

    initialised conversions module

---
+++ model/conversions.py
@@ -0,0 +1,10 @@
+"""Conversion module.
+
+Provides useful conversions for the following:
+    energy conversions
+    volume conversions
+    mass conversions
+    distance conversions
+    land area conversions
+    inflation-adjusted-dollars
+"""
