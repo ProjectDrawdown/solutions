343dd21bc07a6cde0fc6592eaec87555a6a293af
ssmssam <ssmssam@gmail.com>
2021-07-26 07:25:14 +0100

    added test_terawatt_to_kilowatt

--- model/tests/test_conversions.py
+++ model/tests/test_conversions.py
@@ -11,3 +11,13 @@ from model import conversions
 def test_mha_to_ha(in_mha, expected_in_ha):
     converted_result = conversions.mha_to_ha(in_mha)
     assert converted_result == expected_in_ha
+
+
+@pytest.mark.parametrize('in_tw,expected_in_kw',
+                         [(32, 32_000_000_000),
+                          (1, 1_000_000_000),
+                          (0, 0),
+                          (-3, -3_000_000_000)])
+def test_terawatt_to_kilowatt(in_tw, expected_in_kw):
+    converted_result = conversions.terawatt_to_kilowatt(in_tw)
+    assert converted_result == expected_in_kw
