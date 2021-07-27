a04bc920e3212193734e293433eafeec3e2bffb7
ssmssam <ssmssam@gmail.com>
2021-07-27 08:58:27 +0100

    created metaclass which enery conversion now inherits from

--- model/conversions.py
+++ model/conversions.py
@@ -10,6 +10,9 @@ Provides useful conversions for the following:
 """
 
 
+from abc import ABC, abstractmethod
+
+
 def mha_to_ha(mha=1):
     """Convert mega hectares to hectares
         mha : the number of mega hectares
@@ -24,38 +27,40 @@ def terawatt_to_kilowatt(tw=1):
     return tw * 10**9
 
 
-class EnergyConversion():
-    """Convert units from one form 'convert_from' to another 'convert_to'.
+class Conversion(ABC):
+    """
+    A base-class that all conversion classes can inherit from.
+    For each class, need to define the accepted names, the base unit,
+    and the conversion rates to and from this base unit.
+
+    Convert units from one form 'convert_from' to another 'convert_to'.
        Can specify the quantity of units to convert but defaults to one.
        Must be in the specified list 'accepted_names'
             convert_from : the starting units to be converted
             convert_to : the resulting units after the conversion
             quantity : the amount of units you have to begin with
-        """
+    """
 
     def __init__(self, convert_from, convert_to, quantity=1):
-        self.accepted_names = (
-        'tj', 'gcal', 'mtoe', 'mbtu', 'gwh', 'twh', 'kwh')
-        self._base_unit = 'tj'
         self.convert_from = convert_from
         self.convert_to = convert_to
-        self.conversion_rates = {
-                                 'tj_to_tj': 1,
-                                 'gcal_to_tj': 0.004187,
-                                 'mtoe_to_tj': 41_868,
-                                 'mbtu_to_tj': 0.001055,
-                                 'gwh_to_tj': 3.6,
-                                 'twh_to_tj': 3_600,
-                                 'kwh_to_tj': 3.60E-06,
-                                 'tj_to_gcal': 238.845897,
-                                 'tj_to_mtoe': 0.000024,
-                                 'tj_to_mbtu': 947.817120,
-                                 'tj_to_gwh': 0.277778,
-                                 'tj_to_twh': 0.000278,
-                                 'tj_to_kwh': 277_777.778
-                                 }
         self.quantity = quantity
         self._converted_quantity = None
+
+    @property
+    @abstractmethod
+    def accepted_names(self):
+        pass
+
+    @property
+    @abstractmethod
+    def _base_unit(self):
+        pass
+
+    @property
+    @abstractmethod
+    def conversion_rates(self):
+        pass
 
     def __call__(self, quantity=1):
         return self.converted_quantity * quantity
@@ -86,8 +91,9 @@ class EnergyConversion():
 
     def test_accepted_unit(self, unit_name):
         if unit_name not in self.accepted_names:
-            raise ValueError(f'The supplied unit name {unit_name} is not an accepted '
-                             f'unit name. Accepted names are {self.accepted_names}')
+            raise ValueError(
+                f'The supplied unit name {unit_name} is not an accepted '
+                f'unit name. Accepted names are {self.accepted_names}')
 
     def convert_units(self):
         """
@@ -100,8 +106,41 @@ class EnergyConversion():
         base_key_to_search = f'{self.convert_from}_to_{self._base_unit}'
         conversion_key_to_search = f'{self._base_unit}_to_{self.convert_to}'
         base_unit_quantity = self.conversion_rates.get(base_key_to_search)
-        conversion_rate = self.conversion_rates.get(conversion_key_to_search)
+        conversion_rate = self.conversion_rates.get(
+            conversion_key_to_search)
         return self.quantity * conversion_rate * base_unit_quantity
+
+
+class EnergyConversion(Conversion):
+    """
+    Convert between 'tj', 'gcal', 'mtoe', 'mbtu', 'gwh', 'twh', 'kwh'
+    """
+    @property
+    def accepted_names(self):
+        return ('tj', 'gcal', 'mtoe', 'mbtu', 'gwh', 'twh', 'kwh')
+
+    @property
+    def _base_unit(self):
+        return 'tj'
+
+    @property
+    def conversion_rates(self):
+        return {
+        'tj_to_tj': 1,
+        'gcal_to_tj': 0.004187,
+        'mtoe_to_tj': 41_868,
+        'mbtu_to_tj': 0.001055,
+        'gwh_to_tj': 3.6,
+        'twh_to_tj': 3_600,
+        'kwh_to_tj': 3.60E-06,
+        'tj_to_gcal': 238.845897,
+        'tj_to_mtoe': 0.000024,
+        'tj_to_mbtu': 947.817120,
+        'tj_to_gwh': 0.277778,
+        'tj_to_twh': 0.000278,
+        'tj_to_kwh': 277_777.778
+    }
+
 
 
 #todo remove this
