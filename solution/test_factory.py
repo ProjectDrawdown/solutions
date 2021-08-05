15f1167c1d2f9c78a3ea77f538fb5690a552272f
Denise Draper <draperd@acm.org>
2021-08-04 23:28:14 -0700

    remove outdated test

--- solution/test_factory.py
+++ solution/test_factory.py
@@ -19,10 +19,4 @@ def test_one_solution_scenarios():
     assert constructor is not None
     assert len(scenarios) > 0
 
-def test_all_solutions_present():
-    all_solutions = pd.read_csv(os.path.join('data', 'overview', 'solutions.csv'),
-            index_col=False, skipinitialspace=True, header=0,
-            skip_blank_lines=True, comment='#')
-    factory_solutions = factory.all_solutions_scenarios()
-    for (_, val) in all_solutions['DirName'].dropna().iteritems():
-        assert val in factory_solutions.keys()
+
