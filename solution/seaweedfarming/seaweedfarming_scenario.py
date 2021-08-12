fcd16180db752dbf039a8c73568217626d2fc9a1
glass-wing <neilmlyons@gmail.com>
2021-08-12 14:14:16 +0900

    had to reinstate some attributes as a result of retiring the OceanScenario class.

---
+++ solution/seaweedfarming/seaweedfarming_scenario.py
@@ -0,0 +1,26 @@
+
+from dataclasses import dataclass, field
+from model.scenario import Scenario
+    
+@dataclass
+class SeaweedFarmingScenario(Scenario):
+
+    # Current Adoption:
+    adoption_value_world : float = field(metadata={'Units': 'Millions of Ha'})
+
+    sequestration_rate_all_ocean : float  = field(metadata={'Units': 't C/ha/year'})
+    disturbance_rate : float = field(metadata={'Units': 'percentage lost annually'})
+
+    # General Ocean Inputs
+    new_growth_harvested_every : float = field(metadata={'Units': 'years'})
+    allow_disturbance_to_increase_op_costs : bool
+
+    # Current Adoption:
+    adoption_value_world : float = field(metadata={'Units': 'Millions of Ha'})
+
+    ## Following not defaulted - may be added after initialisation.
+
+    # Ref Adoption Scenario Inputs:
+    ref_scenario_name : str
+    ref_base_custom_adoption_on : str
+    ref_adoption_use_only_regional_data : bool
