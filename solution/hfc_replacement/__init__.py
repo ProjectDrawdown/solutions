2ebe9e12316dd7ed7157bbc686cc9c8b722e80b5
danielmk <danielmuellermsc@gmail.com>
2021-08-14 11:33:57 +0200

    Avoid ca not defined in __init__

---
+++ solution/hfc_replacement/__init__.py
@@ -0,0 +1,291 @@
+"""Refrigerant Management - HFC Replacement solution model.
+   Excel filename: RRS_HFCReplacement Model_June2020Integration.xlsm
+"""
+
+import pathlib
+
+import numpy as np
+import pandas as pd
+
+from model import adoptiondata
+from model import advanced_controls as ac
+from model import ch4calcs
+from model import co2calcs
+from model import customadoption
+from model import dd
+from model import emissionsfactors
+from model import firstcost
+from model import helpertables
+from model import operatingcost
+from model import s_curve
+from model import scenario
+from model import unitadoption
+from model import vma
+from model import tam
+from solution import rrs
+
+DATADIR = pathlib.Path(__file__).parents[2].joinpath('data')
+THISDIR = pathlib.Path(__file__).parents[0]
+VMAs = {
+    'Current Adoption': vma.VMA(
+        filename=THISDIR.joinpath("vma_data", "Current_Adoption.csv"),
+        use_weight=False),
+    'CONVENTIONAL First Cost per Implementation Unit': vma.VMA(
+        filename=None, use_weight=False),
+    'SOLUTION First Cost per Implementation Unit': vma.VMA(
+        filename=None, use_weight=False),
+    'CONVENTIONAL Lifetime Capacity': vma.VMA(
+        filename=None, use_weight=False),
+    'SOLUTION Lifetime Capacity': vma.VMA(
+        filename=None, use_weight=False),
+    'CONVENTIONAL Average Annual Use': vma.VMA(
+        filename=None, use_weight=False),
+    'SOLUTION Average Annual Use': vma.VMA(
+        filename=None, use_weight=False),
+    'CONVENTIONAL Variable Operating Cost (VOM) per Functional Unit': vma.VMA(
+        filename=None, use_weight=False),
+    'SOLUTION Variable Operating Cost (VOM) per Functional Unit': vma.VMA(
+        filename=None, use_weight=False),
+    'CONVENTIONAL Fixed Operating Cost (FOM)': vma.VMA(
+        filename=None, use_weight=False),
+    'SOLUTION Fixed Operating Cost (FOM)': vma.VMA(
+        filename=None, use_weight=False),
+    'CONVENTIONAL Total Energy Used per Functional Unit': vma.VMA(
+        filename=None, use_weight=False),
+    'SOLUTION Energy Efficiency Factor': vma.VMA(
+        filename=None, use_weight=False),
+    'SOLUTION Total Energy Used per Functional Unit': vma.VMA(
+        filename=None, use_weight=False),
+    'CONVENTIONAL Fuel Consumed per Functional Unit': vma.VMA(
+        filename=None, use_weight=False),
+    'SOLUTION Fuel Efficiency Factor': vma.VMA(
+        filename=None, use_weight=False),
+    'CONVENTIONAL Direct Emissions per Functional Unit': vma.VMA(
+        filename=THISDIR.joinpath("vma_data", "CONVENTIONAL_Direct_Emissions_per_Functional_Unit.csv"),
+        use_weight=False),
+    'SOLUTION Direct Emissions per Functional Unit': vma.VMA(
+        filename=None, use_weight=False),
+    'CONVENTIONAL Indirect CO2 Emissions per Unit': vma.VMA(
+        filename=None, use_weight=False),
+    'SOLUTION Indirect CO2 Emissions per Unit': vma.VMA(
+        filename=None, use_weight=False),
+    'CH4-CO2eq Tons Reduced': vma.VMA(
+        filename=None, use_weight=False),
+    'N2O-CO2eq Tons Reduced': vma.VMA(
+        filename=None, use_weight=False),
+    'CONVENTIONAL Revenue per Functional Unit': vma.VMA(
+        filename=None, use_weight=False),
+    'SOLUTION Revenue per Functional Unit': vma.VMA(
+        filename=None, use_weight=False),
+}
+vma.populate_fixed_summaries(vma_dict=VMAs, filename=THISDIR.joinpath('vma_data', 'VMA_info.csv'))
+
+units = {
+    "implementation unit": "kton HFC replaced",
+    "functional unit": "kton refrigerant",
+    "first cost": "US$B",
+    "operating cost": "US$B",
+}
+
+name = 'Refrigerant Management - HFC Replacement'
+solution_category = ac.SOLUTION_CATEGORY.REDUCTION
+
+scenarios = ac.load_scenarios_from_json(directory=THISDIR.joinpath('ac'), vmas=VMAs)
+
+
+class Scenario(scenario.Scenario):
+    name = name
+    units = units
+    vmas = VMAs
+    solution_category = solution_category
+
+    def __init__(self, scenario=None):
+        if scenario is None:
+            scenario = list(scenarios.keys())[0]
+        self.scenario = scenario
+        self.ac = scenarios[scenario]
+
+        # TAM
+        tamconfig_list = [
+            ['param', 'World', 'PDS World', 'OECD90', 'Eastern Europe', 'Asia (Sans Japan)',
+                'Middle East and Africa', 'Latin America', 'China', 'India', 'EU', 'USA'],
+            ['source_until_2014', self.ac.source_until_2014, self.ac.source_until_2014,
+                'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES',
+                'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES' ],
+            ['source_after_2014', self.ac.ref_source_post_2014, self.ac.pds_source_post_2014,
+                'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES',
+                'ALL SOURCES', 'ALL SOURCES', 'ALL SOURCES' ],
+            ['trend', '3rd Poly', '3rd Poly', '3rd Poly', '3rd Poly', '3rd Poly',
+              '3rd Poly', '3rd Poly', '3rd Poly', '3rd Poly', '3rd Poly', '3rd Poly'],
+            ['growth', 'High', 'High', 'High', 'High', 'High',
+              'High', 'High', 'High', 'High', 'High', 'High'],
+            ['low_sd_mult', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            ['high_sd_mult', 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]]
+        tamconfig = pd.DataFrame(tamconfig_list[1:], columns=tamconfig_list[0]).set_index('param')
+        tam_ref_data_sources = {
+              'Baseline Cases': {
+                  'Mean of Velders 2015 Upper and Lower HFC Emissions Scenarios': THISDIR.joinpath('tam', 'tam_Mean_of_Velders_2015_Upper_and_Lower_HFC_Emissions_Scenarios.csv'),
+            },
+              'Conservative Cases': {
+                  'Velders 2015 Upper  HFC Emissions Scenarios': THISDIR.joinpath('tam', 'tam_Velders_2015_Upper_HFC_Emissions_Scenarios.csv'),
+            },
+              'Ambitious Cases': {
+                  'Velders 2015 Lower  HFC Emissions Scenarios': THISDIR.joinpath('tam', 'tam_Velders_2015_Lower_HFC_Emissions_Scenarios.csv'),
+            },
+        }
+        self.tm = tam.TAM(tamconfig=tamconfig, tam_ref_data_sources=tam_ref_data_sources,
+            tam_pds_data_sources=tam_ref_data_sources)
+        ref_tam_per_region=self.tm.ref_tam_per_region()
+        pds_tam_per_region=self.tm.pds_tam_per_region()
+
+        adconfig_list = [
+            ['param', 'World', 'OECD90', 'Eastern Europe', 'Asia (Sans Japan)',
+             'Middle East and Africa', 'Latin America', 'China', 'India', 'EU', 'USA'],
+            ['trend', self.ac.soln_pds_adoption_prognostication_trend, '3rd Poly',
+             '3rd Poly', '3rd Poly', '3rd Poly', '3rd Poly', '3rd Poly',
+             '3rd Poly', '3rd Poly', '3rd Poly'],
+            ['growth', self.ac.soln_pds_adoption_prognostication_growth, 'Medium',
+             'Medium', 'Medium', 'Medium', 'Medium', 'Medium',
+             'Medium', 'Medium', 'Medium'],
+            ['low_sd_mult', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            ['high_sd_mult', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+        adconfig = pd.DataFrame(adconfig_list[1:], columns=adconfig_list[0]).set_index('param')
+        ad_data_sources = {
+            'World': {
+                'lowGWP-lower': THISDIR.joinpath('ad', 'ad_lowGWPlower_Velders_et_al_2015.csv'),
+                'lowGWP-upper': THISDIR.joinpath('ad', 'ad_lowGWPupper_Velders_et_al_2015.csv'),
+                'Mean lowGWP': THISDIR.joinpath('ad', 'ad_Mean_lowGWP_Velders_et_al_2015.csv'),
+            },
+        }
+        self.ad = adoptiondata.AdoptionData(ac=self.ac, data_sources=ad_data_sources,
+            adconfig=adconfig)
+
+        # Custom PDS Data
+        ca_pds_data_sources = [
+            {'name': 'PDS 2 Median Adoption of LowGWP',
+              'description': (
+                    '[PLEASE DESCRIBE IN DETAIL  THE METHODOLOGY YOU USED IN THIS ANALYSIS. BE '
+                    'SURE TO INCLUDE ANY ADDITIONAL EQUATIONS YOU UTILIZED] '
+                    ),
+              'filename': THISDIR.joinpath('ca_pds_data', 'custom_pds_ad_PDS_2_Median_Adoption_of_LowGWP.csv')},
+            {'name': 'PDS 1 Lower Adoption LowGWP',
+              'description': (
+                    '[PLEASE DESCRIBE IN DETAIL  THE METHODOLOGY YOU USED IN THIS ANALYSIS. BE '
+                    'SURE TO INCLUDE ANY ADDITIONAL EQUATIONS YOU UTILIZED] '
+                    ),
+              'filename': THISDIR.joinpath('ca_pds_data', 'custom_pds_ad_PDS_1_Lower_Adoption_LowGWP.csv')},
+            {'name': 'PDS 3 Upper Adoption of LowGWP',
+              'description': (
+                    '[PLEASE DESCRIBE IN DETAIL  THE METHODOLOGY YOU USED IN THIS ANALYSIS. BE '
+                    'SURE TO INCLUDE ANY ADDITIONAL EQUATIONS YOU UTILIZED] '
+                    ),
+              'filename': THISDIR.joinpath('ca_pds_data', 'custom_pds_ad_PDS_3_Upper_Adoption_of_LowGWP.csv')},
+        ]
+        self.pds_ca = customadoption.CustomAdoption(data_sources=ca_pds_data_sources,
+            soln_adoption_custom_name=self.ac.soln_pds_adoption_custom_name,
+            high_sd_mult=self.ac.soln_pds_adoption_custom_high_sd_mult,
+            low_sd_mult=self.ac.soln_pds_adoption_custom_low_sd_mult,
+            total_adoption_limit=pds_tam_per_region)
+
+        ref_adoption_data_per_region = None
+
+        if False:
+            # One may wonder why this is here. This file was code generated.
+            # This 'if False' allows subsequent conditions to all be elif.
+            pass
+        elif self.ac.soln_pds_adoption_basis == 'Fully Customized PDS':
+            pds_adoption_data_per_region = self.pds_ca.adoption_data_per_region()
+            pds_adoption_trend_per_region = self.pds_ca.adoption_trend_per_region()
+            pds_adoption_is_single_source = None
+        elif self.ac.soln_pds_adoption_basis == 'Existing Adoption Prognostications':
+            pds_adoption_data_per_region = self.ad.adoption_data_per_region()
+            pds_adoption_trend_per_region = self.ad.adoption_trend_per_region()
+            pds_adoption_is_single_source = self.ad.adoption_is_single_source()
+
+        ht_ref_adoption_initial = pd.Series(
+            list(self.ac.ref_base_adoption.values()), index=dd.REGIONS)
+        # even when the final_datapoint_year is 2018, the TAM initial year is usually hard-coded to 2014
+        # if that is wrong, change 2014 to 2018 below
+        ht_ref_adoption_final = ref_tam_per_region.loc[2050] * (ht_ref_adoption_initial /
+            ref_tam_per_region.loc[2014])
+        ht_ref_datapoints = pd.DataFrame(columns=dd.REGIONS)
+        ht_ref_datapoints.loc[2018] = ht_ref_adoption_initial
+        ht_ref_datapoints.loc[2050] = ht_ref_adoption_final.fillna(0.0)
+        ht_pds_adoption_initial = ht_ref_adoption_initial
+        ht_pds_adoption_final_percentage = pd.Series(
+            list(self.ac.pds_adoption_final_percentage.values()),
+            index=list(self.ac.pds_adoption_final_percentage.keys()))
+        ht_pds_adoption_final = ht_pds_adoption_final_percentage * pds_tam_per_region.loc[2050]
+        ht_pds_datapoints = pd.DataFrame(columns=dd.REGIONS)
+        ht_pds_datapoints.loc[2018] = ht_pds_adoption_initial
+        ht_pds_datapoints.loc[2050] = ht_pds_adoption_final.fillna(0.0)
+        self.ht = helpertables.HelperTables(ac=self.ac,
+            ref_datapoints=ht_ref_datapoints, pds_datapoints=ht_pds_datapoints,
+            pds_adoption_data_per_region=pds_adoption_data_per_region,
+            ref_adoption_limits=ref_tam_per_region, pds_adoption_limits=pds_tam_per_region,
+            use_first_pds_datapoint_main=False,
+            adoption_base_year=2018,
+            copy_pds_to_ref=False,
+            copy_ref_datapoint=False, copy_pds_datapoint=False, 
+            pds_adoption_trend_per_region=pds_adoption_trend_per_region,
+            pds_adoption_is_single_source=pds_adoption_is_single_source)
+
+        self.ef = emissionsfactors.ElectricityGenOnGrid(ac=self.ac, grid_emissions_version=4)
+
+        self.ua = unitadoption.UnitAdoption(ac=self.ac,
+            ref_total_adoption_units=ref_tam_per_region,
+            pds_total_adoption_units=pds_tam_per_region,
+            soln_ref_funits_adopted=self.ht.soln_ref_funits_adopted(),
+            soln_pds_funits_adopted=self.ht.soln_pds_funits_adopted(),
+            repeated_cost_for_iunits=False,
+            bug_cfunits_double_count=False)
+        soln_pds_tot_iunits_reqd = self.ua.soln_pds_tot_iunits_reqd()
+        soln_ref_tot_iunits_reqd = self.ua.soln_ref_tot_iunits_reqd()
+        conv_ref_tot_iunits = self.ua.conv_ref_tot_iunits()
+        soln_net_annual_funits_adopted=self.ua.soln_net_annual_funits_adopted()
+
+        self.fc = firstcost.FirstCost(ac=self.ac, pds_learning_increase_mult=2,
+            ref_learning_increase_mult=2, conv_learning_increase_mult=2,
+            soln_pds_tot_iunits_reqd=soln_pds_tot_iunits_reqd,
+            soln_ref_tot_iunits_reqd=soln_ref_tot_iunits_reqd,
+            conv_ref_tot_iunits=conv_ref_tot_iunits,
+            soln_pds_new_iunits_reqd=self.ua.soln_pds_new_iunits_reqd(),
+            soln_ref_new_iunits_reqd=self.ua.soln_ref_new_iunits_reqd(),
+            conv_ref_new_iunits=self.ua.conv_ref_new_iunits(),
+            fc_convert_iunit_factor=1)
+
+        self.oc = operatingcost.OperatingCost(ac=self.ac,
+            soln_net_annual_funits_adopted=soln_net_annual_funits_adopted,
+            soln_pds_tot_iunits_reqd=soln_pds_tot_iunits_reqd,
+            soln_ref_tot_iunits_reqd=soln_ref_tot_iunits_reqd,
+            conv_ref_annual_tot_iunits=self.ua.conv_ref_annual_tot_iunits(),
+            soln_pds_annual_world_first_cost=self.fc.soln_pds_annual_world_first_cost(),
+            soln_ref_annual_world_first_cost=self.fc.soln_ref_annual_world_first_cost(),
+            conv_ref_annual_world_first_cost=self.fc.conv_ref_annual_world_first_cost(),
+            single_iunit_purchase_year=2017,
+            soln_pds_install_cost_per_iunit=self.fc.soln_pds_install_cost_per_iunit(),
+            conv_ref_install_cost_per_iunit=self.fc.conv_ref_install_cost_per_iunit(),
+            conversion_factor=1)
+
+        self.c4 = ch4calcs.CH4Calcs(ac=self.ac,
+            soln_net_annual_funits_adopted=soln_net_annual_funits_adopted)
+
+        self.c2 = co2calcs.CO2Calcs(ac=self.ac,
+            ch4_ppb_calculator=self.c4.ch4_ppb_calculator(),
+            soln_pds_net_grid_electricity_units_saved=self.ua.soln_pds_net_grid_electricity_units_saved(),
+            soln_pds_net_grid_electricity_units_used=self.ua.soln_pds_net_grid_electricity_units_used(),
+            soln_pds_direct_co2_emissions_saved=self.ua.soln_pds_direct_co2_emissions_saved(),
+            soln_pds_direct_ch4_co2_emissions_saved=self.ua.soln_pds_direct_ch4_co2_emissions_saved(),
+            soln_pds_direct_n2o_co2_emissions_saved=self.ua.soln_pds_direct_n2o_co2_emissions_saved(),
+            soln_pds_new_iunits_reqd=self.ua.soln_pds_new_iunits_reqd(),
+            soln_ref_new_iunits_reqd=self.ua.soln_ref_new_iunits_reqd(),
+            conv_ref_new_iunits=self.ua.conv_ref_new_iunits(),
+            conv_ref_grid_CO2_per_KWh=self.ef.conv_ref_grid_CO2_per_KWh(),
+            conv_ref_grid_CO2eq_per_KWh=self.ef.conv_ref_grid_CO2eq_per_KWh(),
+            soln_net_annual_funits_adopted=soln_net_annual_funits_adopted,
+            fuel_in_liters=False)
+
+        self.r2s = rrs.RRS(total_energy_demand=ref_tam_per_region.loc[2014, 'World'],
+            soln_avg_annual_use=self.ac.soln_avg_annual_use,
+            conv_avg_annual_use=self.ac.conv_avg_annual_use)
+
