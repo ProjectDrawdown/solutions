1425e495d03e8ea721535ff433f4664912a5c499
glass-wing <neilmlyons@gmail.com>
2021-08-27 16:14:17 +0900

    tests working now

--- solution/seafloorprotection/tests/test_seafloorprotection.py
+++ solution/seafloorprotection/tests/test_seafloorprotection.py
@@ -1,51 +1,69 @@
 
 import pytest
 
-from solution.macroalgaerestoration.macroalgaerestoration_solution import MacroalgaeRestorationSolution
+from solution.seafloorprotection.seafloorprotection_solution import SeafloorProtectionSolution
 
-solution_name = 'macroalgaerestoration'
-scenario_name = 'PDS-16p2050-Optimum 20August2021'
-mars = MacroalgaeRestorationSolution()
-mars.load_scenario(scenario_name)
+solution_name = 'seafloorprotection'
+scenario_name = 'PDS-48p2050-Optimum'
+sps = SeafloorProtectionSolution()
+sps.load_scenario(scenario_name)
 
 def test_adoption_unit_increase_pds_vs_ref_final_year():
-    au_inc = mars.get_adoption_unit_increase_pds_vs_ref_final_year()
-    assert au_inc == pytest.approx(33.61214819960)
+    au_inc = sps.get_adoption_unit_increase_pds_vs_ref_final_year()
+    assert au_inc == pytest.approx(441.00)
     
 def test_adoption_unit_increase_pds_final_year():
-    gu_final = mars.get_adoption_unit_increase_pds_final_year()
-    assert gu_final == pytest.approx(33.61214819960)
+    gu_final = sps.get_adoption_unit_increase_pds_final_year()
+    assert gu_final == pytest.approx(441.00)
     
 def test_global_percent_adoption_base_year():
-    gpa_base = mars.get_global_percent_adoption_base_year()
+    gpa_base = sps.get_global_percent_adoption_base_year()
     assert gpa_base == pytest.approx(0.0 / 100)
     
 def test_percent_adoption_start_year():
-    gpa_start = mars.get_percent_adoption_start_year()
-    assert gpa_start == pytest.approx(1.0609895265/ 100)
+    gpa_start = sps.get_percent_adoption_start_year()
+    assert gpa_start == pytest.approx(5.625/ 100)
     
 def test_get_percent_adoption_end_year():
-    gpa_end = mars.get_percent_adoption_end_year()
-    assert gpa_end == pytest.approx(16.9758324240 / 100)
+    gpa_end = sps.get_percent_adoption_end_year()
+    assert gpa_end == pytest.approx(90.00 / 100)
 
 ######
 
+def test_total_emissions_reduction():
+    total_emissions_reduction = sps.get_total_emissions_reduction()
+    assert total_emissions_reduction == pytest.approx(5.91011393970)
+
 def test_total_co2_seq():
-    total_co2_seq = mars.get_total_co2_seq()
-    assert total_co2_seq == pytest.approx(1.952028234)
+    total_co2_seq = sps.get_total_co2_seq()
+    assert total_co2_seq == 0.0
+
+def test_reduced_area_degradation():
+    reduced_area_degradation = sps.get_reduced_area_degradation()
+    assert reduced_area_degradation == 427.21875000
+
+def test_max_annual_emissions_reduction():
+    max_annual_emissions_reduction = sps.get_max_annual_emissions_reduction()
+    assert max_annual_emissions_reduction == pytest.approx(0.19064883676)
+
+def test_emissions_reduction_final_year():
+    emissions_reduction_final_year = sps.get_emissions_reduction_final_year()
+    assert emissions_reduction_final_year == pytest.approx(0.19064883676)
 
 def test_change_in_ppm_equiv():
-    change_in_ppm_equiv = mars.get_change_in_ppm_equiv()
-    assert change_in_ppm_equiv == pytest.approx(0.1651159440)
+    change_in_ppm_equiv = sps.get_change_in_ppm_equiv()
+    assert change_in_ppm_equiv == pytest.approx(0.4607453935)
 
 def test_change_in_ppm_equiv_final():
-    change_in_ppm_equiv_final = mars.get_change_in_ppm_equiv_final_year()
-    assert change_in_ppm_equiv_final == pytest.approx(0.0092167686)
+    change_in_ppm_equiv_final = sps.get_change_in_ppm_equiv_final_year()
+    assert change_in_ppm_equiv_final == pytest.approx(0.01195646244)
 
 def test_max_annual_co2_sequestered():
-    max_annual_co2_sequestered = mars.get_max_annual_co2_sequestered()
-    assert max_annual_co2_sequestered == pytest.approx(0.1189807685)
+    max_annual_co2_sequestered = sps.get_max_annual_co2_sequestered()
+    assert max_annual_co2_sequestered == 0.0
 
 def test_co2_sequestered_final_year():
-    co2_sequestered_final_year = mars.get_co2_sequestered_final_year()
-    assert co2_sequestered_final_year == pytest.approx(0.1189807685)
+    co2_sequestered_final_year = sps.get_co2_sequestered_final_year()
+    assert co2_sequestered_final_year == 0.0
+
+
