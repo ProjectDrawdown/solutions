ea4b2184886b8498ec8c614de08df1024217451c
glass-wing <neilmlyons@gmail.com>
2021-08-27 16:24:24 +0900

    updated with changes from seafloor protection code

--- model/ocean_solution.py
+++ model/ocean_solution.py
@@ -379,8 +379,18 @@ class OceanSolution(Solution):
 
     def get_change_in_ppm_equiv(self, delay_period = 0) -> np.float64:
         
-        pds_sequestration = self.pds_scenario.get_change_in_ppm_equiv_series(self.sequestration_rate_all_ocean, self.disturbance_rate, self.growth_rate_of_ocean_degradation, self.delay_impact_of_protection_by_one_year)
-        ref_sequestration = self.ref_scenario.get_change_in_ppm_equiv_series(self.sequestration_rate_all_ocean, self.disturbance_rate, self.growth_rate_of_ocean_degradation, self.delay_impact_of_protection_by_one_year)
+        pds_sequestration = self.pds_scenario.get_change_in_ppm_equiv_series(
+                        self.sequestration_rate_all_ocean,
+                        self.disturbance_rate,
+                        self.growth_rate_of_ocean_degradation,
+                        self.delay_impact_of_protection_by_one_year,
+                        emissions_reduced_per_land_unit= 0.0)
+        ref_sequestration = self.ref_scenario.get_change_in_ppm_equiv_series(
+                        self.sequestration_rate_all_ocean,
+                        self.disturbance_rate,
+                        self.growth_rate_of_ocean_degradation,
+                        self.delay_impact_of_protection_by_one_year,
+                        emissions_reduced_per_land_unit=0.0)
 
         net_sequestration = (pds_sequestration - ref_sequestration)
         # net_sequestration should now equal 'CO2-eq PPM Calculator' on tab [CO2 Calcs]!$B$224
@@ -395,8 +405,20 @@ class OceanSolution(Solution):
 
     def get_change_in_ppm_equiv_final_year(self) -> np.float64:
                 
-        pds_sequestration = self.pds_scenario.get_change_in_ppm_equiv_series(self.sequestration_rate_all_ocean, self.disturbance_rate, self.growth_rate_of_ocean_degradation, self.delay_impact_of_protection_by_one_year)
-        ref_sequestration = self.ref_scenario.get_change_in_ppm_equiv_series(self.sequestration_rate_all_ocean, self.disturbance_rate, self.growth_rate_of_ocean_degradation, self.delay_impact_of_protection_by_one_year)
+        pds_sequestration = self.pds_scenario.get_change_in_ppm_equiv_series(
+                        self.sequestration_rate_all_ocean,
+                        self.disturbance_rate,
+                        self.growth_rate_of_ocean_degradation,
+                        self.delay_impact_of_protection_by_one_year,
+                        emissions_reduced_per_land_unit=0.0)
+
+        ref_sequestration = self.ref_scenario.get_change_in_ppm_equiv_series(
+                        self.sequestration_rate_all_ocean,
+                        self.disturbance_rate,
+                        self.growth_rate_of_ocean_degradation,
+                        self.delay_impact_of_protection_by_one_year,
+                        emissions_reduced_per_land_unit=0.0)
+                        
         # net_sequestration should equal 'CO2-eq PPM Calculator' on tab [CO2 Calcs]!$B$224
         net_sequestration = (pds_sequestration - ref_sequestration)
         
