8141aa1e4211aeef3e31120beba28d2fe0a6f914
glass-wing <neilmlyons@gmail.com>
2021-09-15 18:26:50 +0900

    removed reference to seafloor protection scenario

--- solution/seafloorprotection/seafloorprotection_solution.py
+++ solution/seafloorprotection/seafloorprotection_solution.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from model.ocean_solution import OceanSolution
 from model.new_unit_adoption import NewUnitAdoption
-from solution.seafloorprotection.seafloorprotection_scenario import SeafloorProtectionScenario
+
 
 class SeafloorProtectionSolution(OceanSolution):
     """ All calculations for Seafloor Protection currently implemented in the OceanSolution base class.
@@ -29,46 +29,3 @@ class SeafloorProtectionSolution(OceanSolution):
 
         super().__init__(configuration_file_name)
         
-
-    # def set_up_area_units(self, unit_adoption: NewUnitAdoption) -> None:
-    #     # This should produce a flat line with y = constant = self.total_area
-    #     unit_adoption.set_area_units_linear(total_area= self.total_area, change_per_period= self.change_per_period, total_area_as_of_period= self.total_area_as_of_period)
-    #     unit_adoption.apply_clip(lower= None, upper= self.total_area)
-        
-
-    # def load_scenario(self, scenario_name: str) -> None:
-
-    #     input_stream = open(self.scenarios_file, 'r')
-    #     scen_dict = json.load(input_stream)
-
-    #     if scenario_name not in scen_dict.keys():
-    #         raise ValueError(f"Unable to find {scenario_name} in scenario file: {self.scenarios_file}")
-        
-    #     scenario = SeafloorProtectionScenario(**scen_dict[scenario_name])
-
-    #     self.scenario = scenario
-
-    #     super()._load_pds_scenario()
-        
-    #     if self.scenario.ref_scenario_name:
-    #         self._load_ref_scenario()
-    #     else:
-    #         # creates a ref scenario with zeroes.
-    #         self.ref_scenario = self.pds_scenario.get_skeleton()
-
-    #     self.pds_scenario.use_tam_for_co2_calcs = True
-    #     self.ref_scenario.use_tam_for_co2_calcs = True
-
-    #     # Set scenario-specific data:        
-    #     self.sequestration_rate_all_ocean = self.scenario.sequestration_rate_all_ocean
-    #     self.emissions_reduced_per_unit_area = self.scenario.emissions_reduced_per_unit_area
-    #     self.npv_discount_rate = self.scenario.npv_discount_rate
-    #     self.growth_rate_of_ocean_degradation = self.scenario.growth_rate_of_ocean_degradation
-        
-    #     self.disturbance_rate = 0.0
-
-    #     # PDS and REF have a similar TAM structure.
-    #     self.set_up_area_units(self.pds_scenario)
-    #     self.set_up_area_units(self.ref_scenario)
-        
-    #     return
