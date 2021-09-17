0808cdd73fd33eed725f10155bfdc9091fd1205a
glass-wing <neilmlyons@gmail.com>
2021-09-17 15:52:17 +0900

    Implemented, all tests working.

---
+++ solution/saltmarshrestoration/saltmarshrestoration_solution.py
@@ -0,0 +1,26 @@
+
+import os
+
+from model.ocean_solution import OceanSolution
+
+
+class SaltmarshRestorationSolution(OceanSolution):
+
+    # Initialize from configuration file:
+    def __init__(self, configuration_file_name = None):
+        """
+            Constructor requires a configuration file named 'saltmarshrestoration_solution_config.yaml'.
+            This should be located in the same directory as the 'saltmarshrestoration_solution.py' module
+
+        """
+
+        if configuration_file_name is None:
+            filename = os.path.basename(__file__)
+            filename = filename.split('.')[0]
+            configuration_file_name = os.path.join(os.path.dirname(__file__), filename + '_config.yaml')
+        
+        if not os.path.isfile(configuration_file_name):
+            raise ValueError(f'Unable to find configuration file {configuration_file_name}.')
+
+        super().__init__(configuration_file_name)
+        
