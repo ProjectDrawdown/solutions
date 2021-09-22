6130cf88718dfac72cf06d5646f011e997633c07
danielmk <danielmuellermsc@gmail.com>
2021-09-22 16:42:35 +0200

    neat dataclass usage

--- integrations/building_integration.py
+++ integrations/building_integration.py
@@ -2,24 +2,26 @@ from dataclasses import dataclass
 from pathlib import Path
 import pandas as pd
 from model import integration
-from .integration_base import *
+from integration_base import *
 
 THISDIR = Path(__file__).parent
-DATADIR = THISDIR/"data"/"msw"
+DATADIR = THISDIR/"data"/"building"
 
 @dataclass
-class waste_integration_state:
+class building_integration_state:
     # This data class holds global variables that are shared between steps.  Embedding it in a class
     # enables us to avoid having to declare 'global' anytime we want to change something.
 
-    cooking_global_tam : pd.DataFrame = None
-    floor_area_global_tam : pd.DataFrame = None
-    households_global_tam : pd.DataFrame = None
-    lighting_global_tam : pd.DataFrame = None
-    roof_area_global_tam : pd.DataFrame = None
-    space_cooling_global_tam : pd.DataFrame = None
-    space_heating_global_tam : pd.DataFrame = None
-    water_heating_global_tam : pd.DataFrame = None
+    cooking_global_tam : pd.DataFrame = pd.read_csv(DATADIR/"cooking_global_tam.csv", index_col="year", squeeze=False)
+    floor_area_global_tam : pd.DataFrame = pd.read_csv(DATADIR/"floor_area_global_tam.csv", index_col="year", squeeze=False)
+    households_global_tam : pd.DataFrame = pd.read_csv(DATADIR/"households_global_tam.csv", index_col="year", squeeze=False)
+    lighting_global_tam : pd.DataFrame = pd.read_csv(DATADIR/"lighting_global_tam.csv", index_col="year", squeeze=False)
+    roof_area_global_tam : pd.DataFrame = pd.read_csv(DATADIR/"roof_area_global_tam.csv", index_col="year", squeeze=False)
+    space_cooling_global_tam : pd.DataFrame = pd.read_csv(DATADIR/"space_cooling_global_tam.csv", index_col="year", squeeze=False)
+    space_heating_global_tam : pd.DataFrame = pd.read_csv(DATADIR/"space_heating_global_tam.csv", index_col="year", squeeze=False)
+    water_heating_global_tam : pd.DataFrame = pd.read_csv(DATADIR/"water_heating_global_tam.csv", index_col="year", squeeze=False)
+
+data_state = building_integration_state()
 
 def integrate():
     """Perform all steps of the integration together."""
