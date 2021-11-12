6296f3cb5188798a0f4dd19e3cb33dbafce9bb05
Denise Draper <draperd@acm.org>
2021-11-12 11:17:19 -0800

    test vma_xls_extract, clean up unused tests/files

--- tools/tests/test_vma_xls_extract.py
+++ tools/tests/test_vma_xls_extract.py
@@ -1,143 +1,54 @@
-import collections
-import pathlib
-
-import numpy as np
+from pathlib import Path
 import pandas as pd
-import pytest
-import tools.vma_xls_extract
+import tools.vma_xls_extract as vxe
 import openpyxl
 
-thisdir = pathlib.Path(__file__).parents[0]
-wb = openpyxl.load_workbook(thisdir.joinpath('silvopasture_vma.xlsx'), data_only=True, keep_links=False)
+thisdir = Path(__file__).parent
+wb = openpyxl.load_workbook(thisdir/'silvopasture_vma.xlsx', data_only=True, keep_links=False)
+ws = wb[vxe.get_vma_sheet(wb)]
+vma_data = vxe.extract_vmas(ws)
+
+def find_vma_by_name(d, name):
+    for dd in d:
+        if dd['name'] == name:
+            return dd
+    else:
+        return None
 
 
+def test_extract_vmas_right_count():
+    """ Check we produce the right number of tables, discarding empty ones"""
+    assert len(vma_data) == 11
 
-def test_make_vma_df_template():
-    df = tools.vma_xls_extract.make_vma_df_template()
-    expected_cols = {'SOURCE ID: Author/Org, Date, Info', 'Link', 'World / Drawdown Region',
-                     'Specific Geographic Location', 'Thermal-Moisture Regime',
-                     'Source Validation Code', 'Year / Date', 'License Code', 'Raw Data Input',
-                     'Original Units', 'Conversion calculation', 'Common Units', 'Weight',
-                     'Assumptions', 'Exclude Data?', 'Crop',
-                     'Closest Matching Standard Crop (by Revenue/ha)'}
-    assert expected_cols == set(df.columns)
-
-
-def test_single_table():
-    vma_r = tools.vma_xls_extract.VMAReader(wb)
-    result, _, _ = vma_r.read_single_table('C48', sheetname='Variable Meta-analysis',
-                                           fixed_summary=False)
-    expected = pd.read_csv(thisdir.joinpath('silvopasture_vma1.csv'))
-    expected['Year / Date'] = expected['Year / Date'].astype('object')
-    pd.testing.assert_frame_equal(expected, result)
-
-
-def test_single_table_use_weight():
-    vma_r = tools.vma_xls_extract.VMAReader(wb)
-    _, uw, _ = vma_r.read_single_table('C48', sheetname='Variable Meta-analysis',
-                                       fixed_summary=False)
-    assert not uw
-
-
-@pytest.mark.slow
-def test_read_xls_num_dfs():
-    """ Check we produce the right amount of tables + discard empty ones """
-    vma_r = tools.vma_xls_extract.VMAReader(wb)
-    vma_df = vma_r.read_xls()
-    assert len(vma_df) == 24
-    non_empty_tables = len(vma_df.loc[(vma_df['Filename'] != '')])
-    assert non_empty_tables == 11
-
-
-@pytest.mark.slow
 def test_read_xls():
     """ Check some specifc values from Silvopasture """
-    vma_r = tools.vma_xls_extract.VMAReader(wb)
-    vma_df = vma_r.read_xls()
-    unique = vma_df['Title on xls'].unique()
-    assert 'SOLUTION Net Profit Margin per Functional Unit per Annum' in unique
-    assert 'Sequestration Rates' in unique
-    assert 'SOLUTION Energy Efficiency Factor' in unique
+    assert find_vma_by_name(vma_data, 'SOLUTION Net Profit Margin per Functional Unit per Annum') is not None
+    assert find_vma_by_name(vma_data, 'Sequestration Rates') is not None
+    assert find_vma_by_name(vma_data, 'Total Energy Used per SOLUTION functional unit') is None
 
+def test_vma_table_data():
+    sr = find_vma_by_name(vma_data, 'Sequestration Rates')
+    df = sr['data']
+    assert len(df) == 28  # there are 28 rows of data
+    assert len(df.columns) == len(vxe.STANDARD_COLUMNS)
+    assert df['Exclude Data?'].sum() == 6  # there are six excluded rows
+    assert pd.api.types.is_integer_dtype(df['Year / Date'])   # we converted years to ints successfully
 
-@pytest.mark.slow
-def test_read_xls_additional_var():
-    """ Check the additional var from Silvopasture """
-    vma_r = tools.vma_xls_extract.VMAReader(wb)
-    vma_df = vma_r.read_xls()
-    s = 'Percent silvopasture area to the total grassland area (including potential)'
-    assert s in vma_df['Title on xls'].unique()
+def test_vma_table_info():
+    ycp = find_vma_by_name(vma_data, 'Yield from CONVENTIONAL Practice')
+    assert ycp['use_weight']
+    assert not ycp['bound_correction']
+    assert ycp['description'].startswith('The SOLAW Report')
+    assert ycp['units'] == 'DM tons fodder/ha/yr'
 
-
-@pytest.mark.slow
-def test_xls_df_dict():
-    """ Check that wb is extracted to a dictionary (component of read_xls) """
-
-    vma_r = tools.vma_xls_extract.VMAReader(wb)
-    df_dict = vma_r.xls_df_dict()
-
-    # Do some basic type and shape checking
-    assert isinstance(df_dict, collections.OrderedDict)
-    for key, value in df_dict.items():
-        # VMA title
-        assert isinstance(key, str)
-        # (df, boolean, (float, float, float))
-        assert isinstance(value, tuple)
-        assert len(value) == 3
-        if value[0] is None:
-            assert value[1] is False
-            assert value[2] == (np.nan, ) * 3
-        else:
-            assert isinstance(value[0], pd.core.frame.DataFrame)
-            assert not value[0].empty
-            assert isinstance(value[1], bool)
-            assert isinstance(value[2], tuple)
-            assert len(value[2]) == 3
-            for number in value[2]:
-                assert isinstance(number, float)
-
-    # Check an arbitrary column
-    assert 'Disturbance Rate' in df_dict.keys()
-
-    # A known number of VMAs are empty in the example xlsx
-    assert sum([value[0] is None for value in df_dict.values()]) == 13
-
-
-@pytest.mark.slow
-def test_explicit_fixed_summaries():
-    """Check that when fixed summaries are requested, they are provided"""
-    wb = openpyxl.load_workbook(thisdir.joinpath('solution_xls_extract_RRS_test_A.xlsm'), data_only=True, keep_links=False)
-    vma_r = tools.vma_xls_extract.VMAReader(wb)
-    tables = vma_r.xls_df_dict(fixed_summary=True)
-    (_,_,summary) = tables['CONVENTIONAL First Cost per Implementation Unit'] 
-    assert summary == pytest.approx((2010.0317085196398, 3373.5568673016687, 646.5065497376106))
-
+    cnp = find_vma_by_name(vma_data, 'CONVENTIONAL Net Profit Margin per Functional Unit per Annum')
+    assert not cnp['use_weight']
+    assert cnp['bound_correction']
+    assert cnp['description'].startswith("Costs are sometimes presented per cow")
+    assert cnp['units'] == 'US$2014/ ha/ yr'
 
 def test_normalize_col_name():
-    vma_r = tools.vma_xls_extract.VMAReader(wb)
-    assert vma_r.normalize_col_name('Conedition calculation') == 'Conversion calculation'
-    assert vma_r.normalize_col_name('Manually Exclude Data?') == 'Exclude Data?'
-    assert vma_r.normalize_col_name('Weight by: Production') == 'Weight'
-
-
-@pytest.mark.slow
-def test_rrs():
-    wb = openpyxl.load_workbook(thisdir.joinpath('solarpvutil_vma.xlsm'), data_only=True, keep_links=False)
-    vma_r = tools.vma_xls_extract.VMAReader(wb)
-    vma_df = vma_r.read_xls()
-    assert vma_df.loc[1, 'Title on xls'] == 'Current Adoption'
-
-
-@pytest.mark.slow
-def test_large_vma():
-    wb = openpyxl.load_workbook(thisdir.joinpath('lg_vma.xlsx'), data_only=True, keep_links=False)
-    vma_r = tools.vma_xls_extract.VMAReader(wb)
-    tables = vma_r.xls_df_dict()
-    assert len(tables) == 26
-
-    # this is one of the large tables
-    assert 'CONVENTIONAL Direct Emissions per Functional Unit' in tables.keys()
-    (df, _, _) = tables['CONVENTIONAL Direct Emissions per Functional Unit']
-    assert (205,14) == df.shape
-    assert df.loc[204]['Raw Data Input'] == pytest.approx(24.8)
+    assert vxe.normalize_col_name('Conedition calculation') == 'Conversion calculation'
+    assert vxe.normalize_col_name('Manually Exclude Data?') == 'Exclude Data?'
+    assert vxe.normalize_col_name('Weight by: Production') == 'Weight'
 
